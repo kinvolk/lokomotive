773efcbfb3998db42c935aff3bfb6ab0755cd292
Kosy Anyanwu <kosy@kinvolk.io>
2018-11-13 17:06:12 +0100

    baremetal: Add baremetal terraform template

---
+++ pkg/install/baremetal/template.go
@@ -0,0 +1,61 @@
+package baremetal
+
+var terraformConfigTmpl = `
+module "bare-metal-{{.ClusterName}}" {
+  source = "{{.Source}}"
+
+  providers = {
+    local    = "local.default"
+    null     = "null.default"
+    template = "template.default"
+    tls      = "tls.default"
+  }
+
+  # bare-metal
+  cluster_name           = "{{.ClusterName}}"
+  matchbox_http_endpoint = "{{.MatchboxHTTPEndpoint}}"
+  os_channel             = "{{.OSChannel}}"
+  os_version             = "{{.OSVersion}}"
+
+  # configuration
+  cached_install     = "{{.CachedInstall}}"
+  k8s_domain_name    = "{{.K8sDomainName}}"
+  ssh_authorized_key = "${file("{{.SSHAuthorizedKey}}")}"
+  asset_dir          = "{{.AssetDir}}"
+
+  # machines
+  controller_names   = ["{{.ControllerName}}"]
+  controller_macs    = ["{{.ControllerMac}}"]
+  controller_domains = ["{{.ControllerDomain}}"]
+  worker_names       = {{.WorkerNames}}
+  worker_macs        = {{.WorkerMacs}}
+  worker_domains     = {{.WorkerDomains}}
+}
+
+provider "matchbox" {
+  endpoint    = "{{.MatchboxEndpoint}}"
+  client_cert = "${file("{{.MatchboxClientCert}}")}"
+  client_key  = "${file("{{.MatchboxClientKey}}")}"
+  ca          = "${file("{{.MatchboxCA}}")}"
+}
+
+provider "local" {
+  version = "~> 1.0"
+  alias   = "default"
+}
+
+provider "null" {
+  version = "~> 1.0"
+  alias   = "default"
+}
+
+provider "template" {
+  version = "~> 1.0"
+  alias   = "default"
+}
+
+provider "tls" {
+  version = "~> 1.0"
+  alias   = "default"
+}
+`
