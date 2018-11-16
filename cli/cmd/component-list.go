ddf758c6ad39b9f7744819f44588da2245204912
Ashijeet Acharya <ashijeet@kinvolk.io>
2018-11-16 14:06:22 +0530

    cmd: Add a new 'component list' sub-command
    This commit fixes #19
    
    Signed-off-by: Ashijeet Acharya <ashijeet@kinvolk.io>

---
+++ cli/cmd/component-list.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	"github.com/kinvolk/lokoctl/pkg/components"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all available components",
+	Run:   runList,
+}
+
+func init() {
+	componentCmd.AddCommand(listCmd)
+}
+
+func runList(cmd *cobra.Command, args []string) {
+	contextLogger := log.WithFields(log.Fields{
+		"command": "lokoctl component list",
+	})
+
+	if len(args) != 0 {
+		contextLogger.Fatalf("Unknown argument provided for list")
+	}
+
+	fmt.Println("List of available components:")
+	for _, component := range components.List() {
+		fmt.Println("\t", component)
+	}
+}
