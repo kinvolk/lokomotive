17e4b497f61ba892cf0516a6d9eb8207a044ea7c
Kosy Anyanwu <kosy@kinvolk.io>
2018-11-19 11:32:26 +0100

    terraform: Execute, Init and Apply
    Add functionality to execute Terraform in a given path

---
+++ pkg/terraform/install.go
@@ -0,0 +1,47 @@
+package terraform
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/hpcloud/tail"
+	"github.com/pkg/errors"
+)
+
+func executeTerraform(ex *Executor, args ...string) error {
+	pid, done, err := ex.Execute(args...)
+	if err != nil {
+		return errors.Wrapf(err, "failed to run 'terraform %s'", strings.Join(args, " "))
+	}
+
+	pathToFile := filepath.Join(ex.WorkingDirectory(), "logs", fmt.Sprintf("%d%s", pid, ".log"))
+	t, err := tail.TailFile(pathToFile, tail.Config{Follow: true})
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for line := range t.Lines {
+			fmt.Println(line.Text)
+		}
+	}()
+
+	<-done
+	return t.Stop()
+}
+
+// InitAndApply create a new terraform executor for the given path,
+// initializes it and applies the found modules.
+func InitAndApply(exPath string) error {
+	ex, err := NewExecutor(exPath)
+	if err != nil {
+		return errors.Wrap(err, "failed to create terraform executor")
+	}
+
+	if err := executeTerraform(ex, "init", "-no-color"); err != nil {
+		return err
+	}
+
+	return executeTerraform(ex, "apply", "-auto-approve", "-no-color")
+}
