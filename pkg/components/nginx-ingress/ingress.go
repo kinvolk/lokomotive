fdf831ce2809c386084ee26e53232052f9c9d58e
Suraj Deshmukh <suraj@kinvolk.io>
2018-11-21 15:10:53 +0530

    components: remove the unnecessay code
    The yaml parser was returning new object while the object was already
    defined. This commit removes that and include the yaml parsing call in
    the caller itself.
    
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- pkg/components/nginx-ingress/ingress.go
+++ pkg/components/nginx-ingress/ingress.go
@@ -23,19 +23,8 @@ type Answers struct {
 // GetValues takes in answers file as array of bytes and returns the renderd
 // value as string, otherwise returns an error
 func (a *Answers) GetValues(data []byte) (string, error) {
-	a, err := readAnswers(data)
-	if err != nil {
+	if err := yaml.Unmarshal(data, a); err != nil {
 		return "", errors.Wrap(err, "could not read the answers file")
 	}
 	return utils.RenderTemplate(values, a)
-}
-
-// readAnswers takes in answer's file contents as byte array and returns parsed
-// Answers object
-func readAnswers(data []byte) (*Answers, error) {
-	var a Answers
-	if err := yaml.Unmarshal(data, &a); err != nil {
-		return nil, err
-	}
-	return &a, nil
 }
