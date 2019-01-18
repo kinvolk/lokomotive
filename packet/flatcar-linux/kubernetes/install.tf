68f77c250df0ebdb1a6b2d41f1591dcbe91024b7
Johannes Liebermann <johannes@kinvolk.io>
2019-01-18 21:33:09 +0100

    Add Route53 config
    We've decided to use Route53 as a DNS provider for Packet for now.
    More providers may be added in the future, possibly including the
    option for the user to self-configure a generic DNS provider.

--- packet/flatcar-linux/kubernetes/install.tf
+++ packet/flatcar-linux/kubernetes/install.tf
@@ -1,5 +1,37 @@
 provider "packet" {}
 
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+# Discrete DNS records for each controller's private IPv4 for etcd usage
+resource "aws_route53_record" "etcds" {
+  count = "${var.controller_count}"
+
+  # DNS Zone where record should be created
+  zone_id = "${var.dns_zone_id}"
+
+  name = "${format("%s-etcd%d.%s.", var.cluster_name, count.index, var.dns_zone)}"
+  type = "A"
+  ttl  = 300
+
+  # private IPv4 address for etcd
+  records = ["${element(packet_device.controllers.*.access_private_ipv4, count.index)}"]
+}
+
+# DNS record for the API servers
+resource "aws_route53_record" "apiservers" {
+  zone_id = "${var.dns_zone_id}"
+
+  name = "${format("%s.%s.", var.cluster_name, var.dns_zone)}"
+  type = "A"
+  ttl  = "300"
+
+  # TODO - figure out a way to access the API servers when they don't have a public IPv4
+  # TODO - verify that a multi-controller setup actually works
+  records = ["${packet_device.controllers.*.access_public_ipv4}"]
+}
+
 resource "packet_device" "controllers" {
   count            = "${var.controller_count}"
   hostname         = "controller-${count.index}"
--- packet/flatcar-linux/kubernetes/variables.tf
+++ packet/flatcar-linux/kubernetes/variables.tf
@@ -1,3 +1,18 @@
+variable "cluster_name" {
+  type        = "string"
+  description = "Unique cluster name (prepended to dns_zone)"
+}
+
+variable "dns_zone" {
+  type        = "string"
+  description = "AWS Route53 DNS Zone (e.g. aws.example.com)"
+}
+
+variable "dns_zone_id" {
+  type        = "string"
+  description = "AWS Route53 DNS Zone ID (e.g. Z3PAABBCFAKEC0)"
+}
+
 variable "project_id" {
   description = "Packet project ID (e.g. 405efe9c-cce9-4c71-87c1-949c290b27dc)"
 }
