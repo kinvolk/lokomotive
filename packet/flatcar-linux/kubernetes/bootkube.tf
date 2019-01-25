9d50c9a43743ce90bd64d666b17c0b82a9c0b22d
Johannes Liebermann <johannes@kinvolk.io>
2019-01-25 19:52:17 +0100

    Add Bootkube module

---
+++ packet/flatcar-linux/kubernetes/bootkube.tf
@@ -0,0 +1,17 @@
+module "bootkube" {
+  source = "git::https://github.com/poseidon/terraform-render-bootkube.git?ref=f382415f2bc18c7a2d39a92fe254e3823a634270"
+
+  cluster_name = "${var.cluster_name}"
+
+  # Cannot use cyclic dependencies on controllers or their DNS records
+  api_servers                     = ["${format("%s.%s", var.cluster_name, var.dns_zone)}"]
+  etcd_servers                    = "${aws_route53_record.etcds.*.name}"
+  asset_dir                       = "${var.asset_dir}"
+  networking                      = "${var.networking}"
+  network_mtu                     = "${var.network_mtu}"
+  network_ip_autodetection_method = "${var.network_ip_autodetection_method}"
+  pod_cidr                        = "${var.pod_cidr}"
+  service_cidr                    = "${var.service_cidr}"
+  cluster_domain_suffix           = "${var.cluster_domain_suffix}"
+  enable_reporting                = "${var.enable_reporting}"
+}
