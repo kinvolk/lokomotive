f49d843bb4ecbd3cc630e09fc99eeccdf395db1f
Michael Schubert <michael@kinvolk.io>
2019-03-18 14:50:59 +0100

    cmd/component-render-manifest: fix panic
    Running `lokoctl component render-manifest` w/o a component name
    currently leads to a panic. Update it to make the behavior identical to
    `lokoctl cluster install`: when a component name is given, render the
    manifests only for that component, otherwise render manifests for all
    components for which we find lokocfg definitions.
    
    Resolves #147

--- cli/cmd/component-render-manifest.go
+++ cli/cmd/component-render-manifest.go
@@ -26,29 +26,47 @@ func runComponentRender(cmd *cobra.Command, args []string) {
 		"args":    args,
 	})
 
-	componentName := args[0]
-	component, err := components.Get(componentName)
-	if err != nil {
-		contextLogger.Fatal(err)
-	}
-
 	lokoConfig, diags := config.LoadConfig("")
 	if len(diags) > 0 {
 		contextLogger.Fatal(diags)
 	}
 
-	componentConfigBody := lokoConfig.LoadComponentConfigBody(componentName)
-
-	if diags := component.LoadConfig(componentConfigBody, lokoConfig.EvalContext); len(diags) > 0 {
-		contextLogger.Fatal(diags)
+	var componentsToRender []string
+	if len(args) > 0 {
+		componentsToRender = append(componentsToRender, args...)
+	} else {
+		for _, component := range lokoConfig.RootConfig.Components {
+			componentsToRender = append(componentsToRender, component.Name)
+		}
 	}
 
-	manifests, err := component.RenderManifests()
-	if err != nil {
+	if err := renderComponentManifests(lokoConfig, componentsToRender...); err != nil {
 		contextLogger.Fatal(err)
 	}
+}
 
-	for filename, manifest := range manifests {
-		fmt.Printf("---\n# %s\n%s", filename, manifest)
+func renderComponentManifests(lokoConfig *config.Config, componentNames ...string) error {
+	for _, componentName := range componentNames {
+		component, err := components.Get(componentName)
+		if err != nil {
+			return err
+		}
+
+		componentConfigBody := lokoConfig.LoadComponentConfigBody(componentName)
+
+		if diags := component.LoadConfig(componentConfigBody, lokoConfig.EvalContext); len(diags) > 0 {
+			return diags
+		}
+
+		manifests, err := component.RenderManifests()
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("# manifests for component %s\n", componentName)
+		for filename, manifest := range manifests {
+			fmt.Printf("---\n# %s\n%s", filename, manifest)
+		}
 	}
+	return nil
 }
