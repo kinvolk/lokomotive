46af48a0a87e4d0346433801b22c1a01bad50bd4
Suraj Deshmukh <suraj@kinvolk.io>
2019-04-10 18:57:32 +0530

    packet: update to latest k8s and kinvolk/terraform-render-bootkube
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- aws/container-linux/kubernetes/bootkube.tf
+++ aws/container-linux/kubernetes/bootkube.tf
@@ -1,6 +1,6 @@
 # Self-hosted Kubernetes assets (kubeconfig, manifests)
 module "bootkube" {
-  source = "github.com/kinvolk/terraform-render-bootkube?ref=d700893c3d5160f34df1601151c533a33aa22e96"
+  source = "github.com/kinvolk/terraform-render-bootkube?ref=5006997d0d950fc55b35d21c43e6d722f7015280"
 
   cluster_name          = "${var.cluster_name}"
   api_servers           = ["${format("%s.%s", var.cluster_name, var.dns_zone)}"]
--- packet/flatcar-linux/kubernetes/bootkube.tf
+++ packet/flatcar-linux/kubernetes/bootkube.tf
@@ -1,20 +1,23 @@
 module "bootkube" {
-  source = "github.com/kinvolk/terraform-render-bootkube?ref=6ef9683afb6d15d0a9545cbb9d478ea5b9528f41"
+  source = "github.com/kinvolk/terraform-render-bootkube?ref=5006997d0d950fc55b35d21c43e6d722f7015280"
 
   cluster_name = "${var.cluster_name}"
 
   # Cannot use cyclic dependencies on controllers or their DNS records
-  api_servers                     = ["${format("%s-private.%s", var.cluster_name, var.dns_zone)}"]
-  api_servers_external            = ["${format("%s.%s", var.cluster_name, var.dns_zone)}"]
-  etcd_servers                    = "${aws_route53_record.etcds.*.name}"
-  asset_dir                       = "${var.asset_dir}"
-  networking                      = "${var.networking}"
-  network_mtu                     = "${var.network_mtu}"
+  api_servers          = ["${format("%s-private.%s", var.cluster_name, var.dns_zone)}"]
+  api_servers_external = ["${format("%s.%s", var.cluster_name, var.dns_zone)}"]
+  etcd_servers         = "${aws_route53_record.etcds.*.name}"
+  asset_dir            = "${var.asset_dir}"
+  networking           = "${var.networking}"
+  network_mtu          = "${var.network_mtu}"
+
   # Select private Packet NIC by using the can-reach Calico autodetection option with the first
   # host in our private CIDR.
   network_ip_autodetection_method = "can-reach=${cidrhost(var.node_private_cidr, 1)}"
-  pod_cidr                        = "${var.pod_cidr}"
-  service_cidr                    = "${var.service_cidr}"
-  cluster_domain_suffix           = "${var.cluster_domain_suffix}"
-  enable_reporting                = "${var.enable_reporting}"
+
+  pod_cidr              = "${var.pod_cidr}"
+  service_cidr          = "${var.service_cidr}"
+  cluster_domain_suffix = "${var.cluster_domain_suffix}"
+  enable_reporting      = "${var.enable_reporting}"
+  enable_aggregation    = "${var.enable_aggregation}"
 }
