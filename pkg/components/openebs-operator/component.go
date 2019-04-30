68ed816948eb1a3fb4eb74c299f1351f68624369
Kosy Anyanwu <kosy@kinvolk.io>
2019-04-30 16:24:14 +0200

    openebs-operator: Setup nodeSelectors for NDM
    Add functionality to optionally specify nodeSelectors for Node Disk Manager (NDM)

--- pkg/components/openebs-operator/component.go
+++ pkg/components/openebs-operator/component.go
@@ -1,11 +1,12 @@
 package openebsoperator
 
 import (
-	"path"
+	"bytes"
+	"html/template"
 
-	"github.com/gobuffalo/packr/v2"
-	"github.com/gobuffalo/packr/v2/file"
+	"github.com/hashicorp/hcl2/gohcl"
 	"github.com/hashicorp/hcl2/hcl"
+	"github.com/pkg/errors"
 
 	"github.com/kinvolk/lokoctl/pkg/components"
 	"github.com/kinvolk/lokoctl/pkg/components/util"
@@ -14,31 +15,39 @@ import (
 const name = "openebs-operator"
 
 func init() {
-	components.Register(name, &component{})
+	components.Register(name, newComponent())
 }
 
-type component struct{}
+type component struct {
+	NDMSelectorLabel string `hcl:"ndm_selector_label,optional"`
+	NDMSelectorValue string `hcl:"ndm_selector_value,optional"`
+}
+
+func newComponent() *component {
+	return &component{}
+}
 
 func (c *component) LoadConfig(configBody *hcl.Body, evalContext *hcl.EvalContext) hcl.Diagnostics {
-	return hcl.Diagnostics{}
+	if configBody == nil {
+		// return empty struct instead of hcl.Diagnostics{components.HCLDiagConfigBodyNil}
+		// since all the component values are optional
+		return hcl.Diagnostics{}
+	}
+	return gohcl.DecodeBody(*configBody, evalContext, c)
 }
 
 func (c *component) RenderManifests() (map[string]string, error) {
-	ret := make(map[string]string)
-
-	var box *packr.Box
-
-	box = packr.New(name, "./manifests/")
-
-	box.Walk(func(f string, content file.File) error {
-		if path.Ext(f) != ".yaml" {
-			return nil
-		}
-
-		ret[f] = content.String()
-		return nil
-	})
-	return ret, nil
+	tmpl, err := template.New("installer").Parse(operatorInstallerTmpl)
+	if err != nil {
+		return nil, errors.Wrap(err, "parse template failed")
+	}
+	var installerBuf bytes.Buffer
+	if err := tmpl.Execute(&installerBuf, c); err != nil {
+		return nil, errors.Wrap(err, "execute template failed")
+	}
+	return map[string]string{
+		"openebs-operator.yml": installerBuf.String(),
+	}, nil
 }
 
 func (c *component) Install(kubeconfig string) error {
