kinvolk/lokomotive

commit 33478afb54da97898d84856f193d9ac156368150

Unify CLI usage messages

cli/cmd/cluster-install.go
@@ -19,12 +19,12 @@ import (
 
 var clusterCmd = &cobra.Command{
 	Use:   "cluster",
-	Short: "Install a Lokomotive cluster and components",
+	Short: "Install Lokomotive cluster and components",
 }
 
 var clusterInstallCmd = &cobra.Command{
 	Use:   "install",
-	Short: "Install a Lokomotive cluster with components",
+	Short: "Install Lokomotive cluster with components",
 	Run:   runClusterInstall,
 }
 

cli/cmd/component-render-manifest.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 

cli/cmd/component.go
@@ -13,7 +13,7 @@ import (
 
 var componentCmd = &cobra.Command{
 	Use:   "component",
-	Short: "Interact with components of a Lokomotive cluster",
+	Short: "Install Lokomotive components",
 }
 
 func init() {

commit 6a5362dc08c6a04f839a49ff7d512c7b2fa5dc17

cli/cmd/cluster-install.go: fix error message on verify install fail
Introduced in 33478afb54da97898d84856f193d9ac156368150.

Since this error message is provider independent, it obviously shouldn't
say 'on Packet'.

Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

cli/cmd/cluster-install.go
@@ -93,7 +93,7 @@ func runClusterInstall(cmd *cobra.Command, args []string) {
 
 	kubeconfigPath := path.Join(assetDir, "auth", "kubeconfig")
 	if err := verifyInstall(kubeconfigPath); err != nil {
-		ctxLogger.Fatalf("Verify cluster installation on Packet: %v", err)
+		ctxLogger.Fatalf("Verify cluster installation: %v", err)
 	}
 
 	var componentsToInstall []string