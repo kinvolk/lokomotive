57999bca2760ec904276854e71b6499826ab9c25
Mateusz Gozdek <mateusz@kinvolk.io>
2019-07-19 20:12:55 +0200

    cli/cmd/component-render-manifest: log all errors before exiting
    Currently, if error occurs in diagnostic, we log single line and exit
    the process afterwards (how 'Fatal()' works).
    
    For user, it would be useful to see all error messages which has been
    produced, so this commits adds looping over diagnostics we received and
    log them as errors.
    
    This makes working with configuration errors much easier, since you can
    see all errors right away, rather than fixing one problem, re-running
    the command, fixing next problem etc.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- cli/cmd/component-render-manifest.go
+++ cli/cmd/component-render-manifest.go
@@ -29,7 +29,10 @@ func runComponentRender(cmd *cobra.Command, args []string) {
 
 	lokoConfig, diags := config.LoadConfig(viper.GetString("lokocfg"), viper.GetString("lokocfg-vars"))
 	if diags.HasErrors() {
-		contextLogger.Fatal(diags)
+		for _, diagnostic := range diags {
+			contextLogger.Error(diagnostic.Summary)
+		}
+		contextLogger.Fatal("Errors found while loading configuration")
 	}
 
 	var componentsToRender []string
@@ -48,6 +51,10 @@ func runComponentRender(cmd *cobra.Command, args []string) {
 
 func renderComponentManifests(lokoConfig *config.Config, componentNames ...string) error {
 	for _, componentName := range componentNames {
+		contextLogger := log.WithFields(log.Fields{
+			"component": componentName,
+		})
+
 		component, err := components.Get(componentName)
 		if err != nil {
 			return err
@@ -56,6 +63,9 @@ func renderComponentManifests(lokoConfig *config.Config, componentNames ...strin
 		componentConfigBody := lokoConfig.LoadComponentConfigBody(componentName)
 
 		if diags := component.LoadConfig(componentConfigBody, lokoConfig.EvalContext); diags.HasErrors() {
+			for _, diagnostic := range diags {
+				contextLogger.Error(diagnostic.Summary)
+			}
 			return diags
 		}
 
