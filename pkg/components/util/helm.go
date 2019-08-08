kinvolk/lokomotive

commit 17a7d39d5abddc8c729afeb9ca4c48563b4b4786

install, components: Use assets pkg instead of packr directly
Another step for isolating a usage of packr to a single place, so it
can be later easily replaced with something else.

pkg/components/util/helm.go
@@ -8,26 +8,26 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/gobuffalo/packd"
-	packr "github.com/gobuffalo/packr/v2"
 	"github.com/pkg/errors"
 	"k8s.io/helm/pkg/chartutil"
 	"k8s.io/helm/pkg/proto/hapi/chart"
 	"k8s.io/helm/pkg/renderutil"
+
+	"github.com/kinvolk/lokoctl/pkg/assets"
 )
 
-// LoadChartFromBox takes in a packr Box and returns a Helm Chart object or an error.
-func LoadChartFromBox(box *packr.Box) (*chart.Chart, error) {
+// LoadChartFromAssets takes in an asset location and returns a Helm
+// Chart object or an error.
+func LoadChartFromAssets(location string) (*chart.Chart, error) {
 	tmpDir, err := ioutil.TempDir("", "lokoctl-chart-")
 	if err != nil {
 		return nil, errors.Wrap(err, "creating temporary dir")
 	}
 	defer os.RemoveAll(tmpDir)
 
-	walk := func(fileName string, file packd.File) error {
-		fileInfo, err := file.FileInfo()
+	walk := func(fileName string, fileInfo os.FileInfo, file io.ReadSeeker, err error) error {
 		if err != nil {
-			return errors.Wrap(err, "extracting file info")
+			return errors.Wrapf(err, "error during walking at %q", fileName)
 		}
 
 		fileName = filepath.Join(tmpDir, fileName)
@@ -50,8 +50,8 @@ func LoadChartFromBox(box *packr.Box) (*chart.Chart, error) {
 		return nil
 	}
 
-	if err := box.WalkPrefix("", walk); err != nil {
-		return nil, errors.Wrap(err, "walking box")
+	if err := assets.Assets.WalkFiles("/lokomotive-kubernetes", walk); err != nil {
+		return nil, errors.Wrap(err, "walking assets")
 	}
 
 	return chartutil.Load(tmpDir)

Makefile
@@ -22,13 +22,11 @@ update-lk-submodule:
 
 .PHONY: packr2
 packr2:
-	cd pkg/components && packr2
-	cd pkg/install && packr2
+	cd pkg/assets && packr2
 
 .PHONY: packr2-clean
 packr2-clean:
-	cd pkg/components && packr2 clean
-	cd pkg/install && packr2 clean
+	cd pkg/assets && packr2 clean
 
 .PHONY: build-slim
 # Once we change CI code to build outside GOPATH, GO111MODULE can be removed, so

pkg/components/cert-manager/component.go
@@ -1,7 +1,8 @@
 package certmanager
 
 import (
-	packr "github.com/gobuffalo/packr/v2"
+	"fmt"
+
 	"github.com/hashicorp/hcl2/gohcl"
 	"github.com/hashicorp/hcl2/hcl"
 	"github.com/pkg/errors"
@@ -45,11 +46,9 @@ func (c *component) LoadConfig(configBody *hcl.Body, evalContext *hcl.EvalContex
 }
 
 func (c *component) RenderManifests() (map[string]string, error) {
-	box := packr.New(name, "../../../assets/components/cert-manager/manifests/")
-
-	helmChart, err := util.LoadChartFromBox(box)
+	helmChart, err := util.LoadChartFromAssets(fmt.Sprintf("/components/%s/manifests", name))
 	if err != nil {
-		return nil, errors.Wrap(err, "load chart from box")
+		return nil, errors.Wrap(err, "load chart from assets")
 	}
 
 	releaseOptions := &chartutil.ReleaseOptions{

pkg/components/contour/component.go
@@ -1,13 +1,17 @@
 package contour
 
 import (
-	"path"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
-	packr "github.com/gobuffalo/packr/v2"
-	"github.com/gobuffalo/packr/v2/file"
 	"github.com/hashicorp/hcl2/gohcl"
 	"github.com/hashicorp/hcl2/hcl"
+	"github.com/pkg/errors"
 
+	"github.com/kinvolk/lokoctl/pkg/assets"
 	"github.com/kinvolk/lokoctl/pkg/components"
 	"github.com/kinvolk/lokoctl/pkg/components/util"
 )
@@ -46,36 +50,35 @@ func (c *component) LoadConfig(configBody *hcl.Body, evalContext *hcl.EvalContex
 
 func (c *component) RenderManifests() (map[string]string, error) {
 	ret := make(map[string]string)
-
-	// XXX: To use dynamic resolution with packr2 path var, we should use
-	// something like this:
-	// https://github.com/gobuffalo/packr/tree/master/v2#dynamic-box-paths
-	//
-	// But that fails using the manifest path as a string variable, for some
-	// reason (need to create a mini reproduction case and report the bug).
-	// However, I found a work around: we can just declare the box pointer
-	// and assign the pointer to a new box on each path (using a different
-	// name for the box). That avoids all issues with "packr2 build" and the
-	// right assets are used on each case.
-	var box *packr.Box
-
-	if c.InstallMode == "deployment" {
-		box = packr.New("contour-deployment", "../../../assets/components/contour/manifests-deployment/")
-	} else if c.InstallMode == "daemonset" {
-		box = packr.New("contour-daemonset", "../../../assets/components/contour/manifests-daemonset/")
-	} else {
+	switch c.InstallMode {
+	case "deployment", "daemonset":
+		break
+	default:
 		// This should not be possible, it was validated during load
 		panic("This is a bug: install_mode was a valid value and it is not a valid value now.")
 	}
 
-	box.Walk(func(f string, content file.File) error {
-		if path.Ext(f) != ".yaml" {
+	walk := func(fileName string, fileInfo os.FileInfo, r io.ReadSeeker, err error) error {
+		if err != nil {
+			return errors.Wrapf(err, "error during walking at %q", fileName)
+		}
+
+		if filepath.Ext(fileName) != ".yaml" {
 			return nil
 		}
 
-		ret[f] = content.String()
+		contents, err := ioutil.ReadAll(r)
+		if err != nil {
+			return errors.Wrapf(err, "failed to read %q", fileName)
+		}
+
+		ret[fileName] = string(contents)
 		return nil
-	})
+	}
+
+	if err := assets.Assets.WalkFiles(fmt.Sprintf("/components/%s/manifests-%s", name, c.InstallMode), walk); err != nil {
+		return nil, errors.Wrap(err, "failed to walk assets")
+	}
 	return ret, nil
 }
 

pkg/components/openebs-default-storage-class/component.go
@@ -1,12 +1,16 @@
 package openebsoperator
 
 import (
-	"path"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
-	"github.com/gobuffalo/packr/v2"
-	"github.com/gobuffalo/packr/v2/file"
 	"github.com/hashicorp/hcl2/hcl"
+	"github.com/pkg/errors"
 
+	"github.com/kinvolk/lokoctl/pkg/assets"
 	"github.com/kinvolk/lokoctl/pkg/components"
 	"github.com/kinvolk/lokoctl/pkg/components/util"
 )
@@ -25,16 +29,28 @@ func (c *component) LoadConfig(configBody *hcl.Body, evalContext *hcl.EvalContex
 
 func (c *component) RenderManifests() (map[string]string, error) {
 	ret := make(map[string]string)
-	box := packr.New(name, "../../../assets/components/openebs-default-storage-class/manifests/")
+	walk := func(fileName string, fileInfo os.FileInfo, r io.ReadSeeker, err error) error {
+		if err != nil {
+			return errors.Wrapf(err, "error during walking at %q", fileName)
+		}
 
-	box.Walk(func(f string, content file.File) error {
-		if path.Ext(f) != ".yaml" {
+		if filepath.Ext(fileName) != ".yaml" {
 			return nil
 		}
 
-		ret[f] = content.String()
+		contents, err := ioutil.ReadAll(r)
+		if err != nil {
+			return errors.Wrapf(err, "failed to read %q", fileName)
+		}
+
+		ret[fileName] = string(contents)
 		return nil
-	})
+	}
+
+	if err := assets.Assets.WalkFiles(fmt.Sprintf("/components/%s/manifests", name), walk); err != nil {
+		return nil, errors.Wrap(err, "failed to walk assets")
+	}
+
 	return ret, nil
 }
 

pkg/components/prometheus-operator/component.go
@@ -1,7 +1,8 @@
 package prometheus
 
 import (
-	packr "github.com/gobuffalo/packr/v2"
+	"fmt"
+
 	"github.com/hashicorp/hcl2/gohcl"
 	"github.com/hashicorp/hcl2/hcl"
 	"github.com/pkg/errors"
@@ -72,11 +73,9 @@ func (c *component) LoadConfig(configBody *hcl.Body, evalContext *hcl.EvalContex
 }
 
 func (c *component) RenderManifests() (map[string]string, error) {
-	box := packr.New(name, "../../../assets/components/prometheus-operator/manifests/")
-
-	helmChart, err := util.LoadChartFromBox(box)
+	helmChart, err := util.LoadChartFromAssets(fmt.Sprintf("/components/%s/manifests", name))
 	if err != nil {
-		return nil, errors.Wrap(err, "load chart from box")
+		return nil, errors.Wrap(err, "load chart from assets")
 	}
 
 	releaseOptions := &chartutil.ReleaseOptions{

pkg/install/install.go
@@ -6,10 +6,9 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/gobuffalo/packd"
-	packr "github.com/gobuffalo/packr/v2"
 	"github.com/pkg/errors"
 
+	"github.com/kinvolk/lokoctl/pkg/assets"
 	"github.com/kinvolk/lokoctl/pkg/util"
 )
 
@@ -46,11 +45,9 @@ func PrepareLokomotiveTerraformModuleAt(path string) error {
 	if pathExists {
 		return fmt.Errorf("directory at %q exists already - aborting", path)
 	}
-	box := packr.New("lokomotive-kubernetes", "../../assets/lokomotive-kubernetes")
-	walk := func(fileName string, file packd.File) error {
-		fileInfo, err := file.FileInfo()
+	walk := func(fileName string, fileInfo os.FileInfo, r io.ReadSeeker, err error) error {
 		if err != nil {
-			return errors.Wrap(err, "failed to extract file info")
+			return errors.Wrapf(err, "error during walking at %q", fileName)
 		}
 
 		fileName = filepath.Join(path, fileName)
@@ -65,14 +62,14 @@ func PrepareLokomotiveTerraformModuleAt(path string) error {
 		}
 		defer targetFile.Close()
 
-		if _, err := io.Copy(targetFile, file); err != nil {
+		if _, err := io.Copy(targetFile, r); err != nil {
 			return errors.Wrap(err, "failed to write file")
 		}
 		return nil
 	}
 
-	if err := box.Walk(walk); err != nil {
-		return errors.Wrap(err, "failed to walk box")
+	if err := assets.Assets.WalkFiles("/lokomotive-kubernetes", walk); err != nil {
+		return errors.Wrap(err, "failed to walk assets")
 	}
 	return nil
 }

commit 426fc2bd4a10cb5a4229613146ca314304622387

pkg/components/util/helm: use location rather than hardcoded path for loading
17a7d39d5abddc8c729afeb9ca4c48563b4b4786 mistakenly hardcoded the path
used for walking assets, where actually the location argument should be
used.

This commit fixes that.

Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

pkg/components/util/helm.go
@@ -27,7 +27,7 @@ func LoadChartFromAssets(location string) (*chart.Chart, error) {
 	// Rendered files could contain secret data, only allow the
 	// current user but not others
 	walk := walkers.CopyingWalker(tmpDir, 0700)
-	if err := assets.Assets.WalkFiles("/lokomotive-kubernetes", walk); err != nil {
+	if err := assets.Assets.WalkFiles(location, walk); err != nil {
 		return nil, errors.Wrap(err, "walking assets")
 	}
 