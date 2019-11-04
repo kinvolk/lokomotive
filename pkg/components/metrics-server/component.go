deab4a66532049cf99ccc97c5ad947c7ffe5bce3
Mateusz Gozdek <mateusz@kinvolk.io>
2019-11-04 16:38:36 +0100

    pkg/components: Add metrics-server to support HPA
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

---
+++ pkg/components/metrics-server/component.go
@@ -0,0 +1,72 @@
+package metricsserver
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/hcl2/gohcl"
+	"github.com/hashicorp/hcl2/hcl"
+	"github.com/pkg/errors"
+	"k8s.io/helm/pkg/chartutil"
+	"k8s.io/helm/pkg/proto/hapi/chart"
+
+	"github.com/kinvolk/lokoctl/pkg/components"
+	"github.com/kinvolk/lokoctl/pkg/components/util"
+)
+
+const name = "metrics-server"
+
+const chartValuesTmpl = `
+`
+
+func init() {
+	components.Register(name, newComponent())
+}
+
+type component struct {
+	Namespace string `hcl:"namespace,optional"`
+}
+
+func newComponent() *component {
+	return &component{
+		Namespace: "kube-system",
+	}
+}
+
+func (c *component) LoadConfig(configBody *hcl.Body, evalContext *hcl.EvalContext) hcl.Diagnostics {
+	if configBody == nil {
+		return hcl.Diagnostics{}
+	}
+
+	return gohcl.DecodeBody(*configBody, evalContext, c)
+}
+
+func (c *component) RenderManifests() (map[string]string, error) {
+	helmChart, err := util.LoadChartFromAssets(fmt.Sprintf("/components/%s", name))
+	if err != nil {
+		return nil, errors.Wrap(err, "load chart from assets")
+	}
+
+	releaseOptions := &chartutil.ReleaseOptions{
+		Name:      name,
+		Namespace: c.Namespace,
+		IsInstall: true,
+	}
+
+	values, err := util.RenderTemplate(chartValuesTmpl, c)
+	if err != nil {
+		return nil, errors.Wrap(err, "render chart values template")
+	}
+
+	chartConfig := &chart.Config{Raw: values}
+
+	renderedFiles, err := util.RenderChart(helmChart, chartConfig, releaseOptions)
+	if err != nil {
+		return nil, errors.Wrap(err, "render chart")
+	}
+
+	return renderedFiles, nil
+}
+
+func (c *component) Install(kubeconfig string) error {
+	return util.Install(c, kubeconfig)
+}
