e7207fc8ed3d191c06e6e8990e6af65aa3619697
Suraj Deshmukh <suraj@kinvolk.io>
2019-11-06 18:50:13 +0530

    e2e: Check if the pod is in CrashLoopBackOff
    This commit edits the e2e util to test if the deployment is up to also
    test if the pods are up and crashing often. In that case the deployment
    was not successful.
    
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- test/components/util/util.go
+++ test/components/util/util.go
@@ -1,12 +1,16 @@
 package util
 
 import (
+	"fmt"
 	"os"
 	"testing"
 	"time"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -49,8 +53,12 @@ func WaitForDaemonSet(t *testing.T, client kubernetes.Interface, ns, name string
 }
 
 func WaitForDeployment(t *testing.T, client kubernetes.Interface, ns, name string, replicas int, retryInterval, timeout time.Duration) {
-	if err := wait.Poll(retryInterval, timeout, func() (done bool, err error) {
-		ds, err := client.AppsV1().Deployments(ns).Get(name, metav1.GetOptions{})
+	var err error
+	var deploy *appsv1.Deployment
+
+	// Check the readiness of the Deployment
+	if err = wait.PollImmediate(retryInterval, timeout, func() (done bool, err error) {
+		deploy, err = client.AppsV1().Deployments(ns).Get(name, metav1.GetOptions{})
 		if err != nil {
 			if k8serrors.IsNotFound(err) {
 				t.Logf("waiting for deployment %s to be available", name)
@@ -59,12 +67,54 @@ func WaitForDeployment(t *testing.T, client kubernetes.Interface, ns, name strin
 			return false, err
 		}
 
-		if int(ds.Status.AvailableReplicas) == replicas {
+		if int(deploy.Status.AvailableReplicas) == replicas {
 			return true, nil
 		}
-		t.Logf("deployment: %s, replicas: %d/%d", name, int(ds.Status.AvailableReplicas), replicas)
+		t.Logf("deployment: %s, replicas: %d/%d", name, int(deploy.Status.AvailableReplicas), replicas)
 		return false, nil
 	}); err != nil {
 		t.Errorf("error while waiting for the deployment: %v", err)
+		return
 	}
+
+	// Check the readiness of the pods
+	labelSet := labels.Set(deploy.Labels)
+	if err := wait.PollImmediate(retryInterval, timeout, func() (done bool, err error) {
+		pods, err := client.CoreV1().Pods(ns).List(metav1.ListOptions{LabelSelector: labelSet.String()})
+		if err != nil {
+			return false, err
+		}
+		pods = filterNonControllerPods(pods)
+		// go through each pod in the returned list and check the readiness status of it
+		for _, pod := range pods.Items {
+			for _, cs := range pod.Status.ContainerStatuses {
+				if cs.RestartCount > 10 {
+					return false, fmt.Errorf("pod: %s, container %s; pod in CrashLoopBackOff", pod.Name, cs.Name)
+				}
+				if !cs.Ready {
+					t.Logf("pod: %s, container %s; container not ready", pod.Name, cs.Name)
+					return false, nil
+				}
+			}
+			t.Logf("pod %s, has all containers in ready state", pod.Name)
+		}
+		t.Logf("all pods for deployment %s, are in ready state", deploy.Name)
+		return true, nil
+	}); err != nil {
+		t.Errorf("error while waiting for the pods: %v", err)
+	}
+}
+
+func filterNonControllerPods(pods *corev1.PodList) *corev1.PodList {
+	var filteredPods []corev1.Pod
+
+	for _, pod := range pods.Items {
+		// The pod that has a controller, has this label
+		if _, ok := pod.Labels["pod-template-hash"]; !ok {
+			continue
+		}
+		filteredPods = append(filteredPods, pod)
+	}
+	pods.Items = filteredPods
+	return pods
 }
