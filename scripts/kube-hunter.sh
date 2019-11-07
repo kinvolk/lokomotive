78571a330b7c45cf7af0eda5f85f652ff9b2f708
Suraj Deshmukh <suraj@kinvolk.io>
2019-11-07 20:10:26 +0530

    e2e: Add kube-hunter configs and script
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- scripts/kube-hunter.sh
+++ scripts/kube-hunter.sh
@@ -1,3 +1,58 @@
 #!/bin/bash
 
-exit 0
+set -euo pipefail
+
+readonly dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly maxtries=200
+
+function check_tries() {
+  if [ "${count}" -gt "${maxtries}" ]; then
+    echo "Reached maximum number of retries."
+    echo "--------------------------------------"
+    echo "info dump:"
+    set -x
+    kubectl get pods,job
+    kubectl get events
+    kubectl logs "${pod_name}"
+    kubectl get "${pod_name}" -o yaml
+    set +x
+    echo "--------------------------------------"
+    exit 1
+  fi
+  count=$((count + 1))
+}
+
+# Wait until the cluster is responsive
+count=0
+until kubectl get nodes; do
+  check_tries
+
+  echo "Waiting for the cluster to be responsive..."
+  sleep 2
+done
+
+# Install the kube-hunter job
+kubectl apply -f "${dir}/kube-hunter-manifests/config.yaml"
+
+# the pod might take sometime to show up
+count=0
+while [ "$(kubectl get pod -o name -l job-name=kube-hunter)" == "" ]; do
+  check_tries
+
+  echo "The pod has not started yet..."
+  sleep 2
+done
+pod_name=$(kubectl get pod -o name -l job-name=kube-hunter)
+
+count=0
+while [ "$(kubectl get "${pod_name}" -o jsonpath='{.status.phase}')" != "Succeeded" ]; do
+  check_tries
+
+  echo "Waiting until the 'kube-hunter' job is complete..."
+  sleep 2
+done
+
+echo "--------------------------------------------------"
+echo "kube-hunter output:"
+echo "--------------------------------------------------"
+kubectl logs "${pod_name}"
