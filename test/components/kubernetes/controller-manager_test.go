eab016467103849944cf54498d4e6d7f4c75411b
Suraj Deshmukh <suraj@kinvolk.io>
2019-11-07 12:28:22 +0530

    e2e: Add tests for controller-manager
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

---
+++ test/components/kubernetes/controller-manager_test.go
@@ -0,0 +1,26 @@
+// +build aws packet
+// +build e2e
+
+package kubernetes
+
+import (
+	"testing"
+	"time"
+
+	testutil "github.com/kinvolk/lokoctl/test/components/util"
+)
+
+func TestControllerManagerDeployment(t *testing.T) {
+	t.Parallel()
+	namespace := "kube-system"
+	deployment := "kube-controller-manager"
+	replicas := 2
+
+	client, err := testutil.CreateKubeClient(t)
+	if err != nil {
+		t.Errorf("could not create Kubernetes client: %v", err)
+	}
+	t.Log("got kubernetes client")
+
+	testutil.WaitForDeployment(t, client, namespace, deployment, replicas, time.Second*5, time.Minute*5)
+}
