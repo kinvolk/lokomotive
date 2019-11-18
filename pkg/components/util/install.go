01b72505f624538ce3d0a72fff2ec0fb70e1f32c
Mateusz Gozdek <mateusz@kinvolk.io>
2019-11-18 13:46:18 +0100

    pkg/components/util/install: add InstallAsRelease method
    This commit adds InstallAsRelease method, which takes existing component
    format we use and installs it on the given cluster as a Helm release,
    into special 'lokomotive-components' namespace.
    
    All resources will be created in the namespace, defined by the component
    intself. Only component metadata is stored in 'lokomotive-components'
    namespace.
    
    Having all components releases stored in special namespace, allows us to
    list them and treat them as a storage for installed components on the
    cluster. Users should be informed not to install any other Helm releases
    into this namespace.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- pkg/components/util/install.go
+++ pkg/components/util/install.go
@@ -1,8 +1,14 @@
 package util
 
 import (
+	"fmt"
 	"time"
 
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/kube"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/clientcmd"
 
 	"github.com/kinvolk/lokoctl/pkg/components"
@@ -22,4 +28,58 @@ func InstallComponent(c components.Component, kubeconfig string) error {
 	)
 
 	return k8sutil.CreateAssets(clientConfig, renderedFiles, 1*time.Minute)
+}
+
+// InstallAsRelease installs a component as a Helm release using a Helm client.
+func InstallAsRelease(name string, c components.Component, kubeconfig string) error {
+	actionConfig := &action.Configuration{}
+
+	cs, err := k8sutil.NewClientset(kubeconfig)
+	if err != nil {
+		return err
+	}
+
+	// Get the namespace in which the component should be created.
+	ns := c.Metadata().Namespace
+	if ns == "" {
+		return fmt.Errorf("component %s namespace is empty", name)
+	}
+
+	// Ensure the namespace in which we create release and resources exists.
+	_, err = cs.CoreV1().Namespaces().Create(&v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: ns,
+		},
+	})
+	if err != nil && !errors.IsAlreadyExists(err) {
+		return err
+	}
+
+	// TODO: Add some logging implementation? We currently just pass an empty function for logging.
+	if err := actionConfig.Init(kube.GetConfig(kubeconfig, "", ns), ns, "secret", func(format string, v ...interface{}) {}); err != nil {
+		return err
+	}
+
+	chart, err := chartFromManifests(name, c)
+	if err != nil {
+		return err
+	}
+
+	if err := chart.Validate(); err != nil {
+		return fmt.Errorf("chart is invalid: %w", err)
+	}
+
+	install := action.NewInstall(actionConfig)
+	install.ReleaseName = name
+	install.Namespace = ns
+
+	// Wait for charts to become ready to avoid race conditions.
+	// TODO: Make this configurable per component.
+	install.Wait = true
+
+	if _, err := install.Run(chart, map[string]interface{}{}); err != nil {
+		return fmt.Errorf("installing component '%s' as chart failed: %w", name, err)
+	}
+
+	return nil
 }
