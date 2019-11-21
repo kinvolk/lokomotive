99fbf7ef97b5957cc3e709d0fcfd83efc9118dd4
Mateusz Gozdek <mateusz@kinvolk.io>
2019-11-21 17:03:40 +0100

    pkg/components/metadata: initial commit
    This commit introduces component's Metadata struct, which purpose is to
    store common information across all components, like namespace where
    they operate, name, version etc.
    
    Currently, let's only store the namespace where each component should
    live, as this is required for helm 3 installation. In the future we
    probably add more fields to it.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

---
+++ pkg/components/metadata.go
@@ -0,0 +1,7 @@
+package components
+
+// Metadata is a struct which represents basic information about the component.
+// It may contain information like name, version, dependencies, namespace, source etc.
+type Metadata struct {
+	Namespace string
+}
