37da54db78b26bf66ee96ab58245687be5c05646
Mateusz Gozdek <mateusz@kinvolk.io>
2019-11-21 16:49:42 +0100

    pkg/component/interface: add some documentation
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- pkg/components/interface.go
+++ pkg/components/interface.go
@@ -4,7 +4,12 @@ import (
 	"github.com/hashicorp/hcl2/hcl"
 )
 
+// Component represents functionality each Lokomotive component should implement.
 type Component interface {
+	// LoadConfig loads specific component configuration from HCL configuration.
+	// If an error occurs, HCL diagnostics are returned.
 	LoadConfig(*hcl.Body, *hcl.EvalContext) hcl.Diagnostics
+	// RenderManifests returns a map of Kubernetes manifests in YAML format, where
+	// the key is the file from which the content comes.
 	RenderManifests() (map[string]string, error)
 }
