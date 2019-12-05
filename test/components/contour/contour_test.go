cdca5715d6b1a6030ad32d754df58438bd24a2c9
Suraj Deshmukh <suraj@kinvolk.io>
2019-12-05 19:43:47 +0530

    e2e test: Add test for contour deployment
    Previously contour was run in daemonset mode in the CI hence tested to
    run as daemonset.
    
    With update to 1.0.0 version contour itself runs as deployment, this
    commit adds test to test it as deployment.
    
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- test/components/contour/contour_test.go
+++ test/components/contour/contour_test.go
@@ -26,3 +26,19 @@ func TestEnvoyDaemonset(t *testing.T) {
 	testutil.WaitForDaemonSet(t, client, namespace, daemonset, replicas, time.Second*5, time.Minute*5)
 	t.Logf("Found required replicas: %d", replicas)
 }
+
+func TestContourDeployment(t *testing.T) {
+	t.Parallel()
+	namespace := "projectcontour"
+	deployment := "contour"
+	replicas := 2
+
+	client, err := testutil.CreateKubeClient(t)
+	if err != nil {
+		t.Errorf("could not create Kubernetes client: %v", err)
+	}
+	t.Log("got kubernetes client")
+
+	testutil.WaitForDeployment(t, client, namespace, deployment, replicas, time.Second*5, time.Minute*5)
+	t.Logf("Found required replicas: %d", replicas)
+}
