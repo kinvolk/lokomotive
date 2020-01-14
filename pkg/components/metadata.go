930938eecb3d64ba6aa12b4de4536a0e2d78e71f
Mateusz Gozdek <mateusz@kinvolk.io>
2020-01-14 18:56:21 +0100

    pkg/components/metadata.go: add Helm field
    If the field is not nil, this indicates, that the component can be
    managed using Helm instead of being installed as plain objects.
    
    In addition, Helm struct will hold information, which may modify how
    Helm actually installs and manages the release.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- pkg/components/metadata.go
+++ pkg/components/metadata.go
@@ -4,4 +4,10 @@ package components
 // It may contain information like name, version, dependencies, namespace, source etc.
 type Metadata struct {
 	Namespace string
+	Helm      *HelmMetadata
+}
+
+// HelmMetadata stores Helm-related information about a component that is needed when managing component using Helm.
+type HelmMetadata struct {
+	Wait bool
 }
