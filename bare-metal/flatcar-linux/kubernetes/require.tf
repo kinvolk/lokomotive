5ed4e7f8588a388622d076ec047165a7612d76d0
Mateusz Gozdek <mateusz@kinvolk.io>
2020-01-17 16:56:13 +0100

    bare-metal/flatcar-linux/kubernetes: add missing version constraints
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- bare-metal/flatcar-linux/kubernetes/require.tf
+++ bare-metal/flatcar-linux/kubernetes/require.tf
@@ -4,9 +4,11 @@ terraform {
   required_version = ">= 0.12.0"
 
   required_providers {
+    ct       = "~> 0.4"
     local    = "~> 1.2"
     null     = "~> 2.1"
     template = "~> 2.1"
     tls      = "~> 2.0"
+    matchbox = "~> 0.3"
   }
 }
