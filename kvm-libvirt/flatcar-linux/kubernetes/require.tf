4bd9317be2eca51330ce27c5225fc70d96cadae0
Mateusz Gozdek <mateusz@kinvolk.io>
2020-01-20 11:40:51 +0100

    kvm-libvirt/flatcar-linux/kubernetes: simplify handling providers
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- kvm-libvirt/flatcar-linux/kubernetes/require.tf
+++ kvm-libvirt/flatcar-linux/kubernetes/require.tf
@@ -2,29 +2,14 @@
 
 terraform {
   required_version = ">= 0.12.0"
-}
 
-provider "ct" {
-  version = "0.4.0"
-}
-
-provider "local" {
-  version = "~> 1.2"
-}
-
-provider "null" {
-  version = "~> 2.1"
-}
-
-provider "template" {
-  version = "~> 2.1"
-}
-
-provider "tls" {
-  version = "~> 2.0"
-}
-
-provider "libvirt" {
-  version = "~> 0.6.0"
-  uri     = "qemu:///system"
+  required_providers {
+    ct       = "= 0.4.0"
+    local    = "~> 1.2"
+    null     = "~> 2.1"
+    template = "~> 2.1"
+    tls      = "~> 2.0"
+    libvirt  = "~> 0.6.0"
+    packet   = "~> 2.7.3"
+  }
 }
