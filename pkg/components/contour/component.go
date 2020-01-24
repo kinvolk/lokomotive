71c19e0bd7ed073a3010e5ffa4f17360dd6a5c45
Imran Pochi <imran@kinvolk.io>
2020-01-24 20:30:15 +0530

    Add new field and annotation to work with envoy.
    - Add new optional field `IngressHosts` to work with external-dns
    
    - Add annotation to envoy-service. ExternalDNS creates A records for
      LoadBalancer services only if this annotation is present.
    
      Its values are populated based on the above mentioned optional field
      "IngressHosts" provided by the user.
    
    Signed-off-by: Imran Pochi <imran@kinvolk.io>

--- pkg/components/contour/component.go
+++ pkg/components/contour/component.go
@@ -2,9 +2,11 @@ package contour
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/gohcl"
+	"github.com/kinvolk/lokoctl/pkg/components/util"
 	"github.com/pkg/errors"
 
 	"github.com/kinvolk/lokoctl/pkg/assets"
@@ -18,10 +20,21 @@ func init() {
 	components.Register(name, newComponent())
 }
 
+// IngressHosts field is added in order to make contour work with ExternalDNS component.
+// Values provided for IngressHosts is used as value for the annotation `external-dns.alpha.kubernetes.io/hostname`
+// This annotation is added to Envoy service.
 type component struct {
 	ServiceMonitor bool `hcl:"service_monitor,optional"`
+	// IngressHosts field is added in order to make contour work with ExternalDNS component.
+	// Values provided for IngressHosts is used as value for the annotation `external-dns.alpha.kubernetes.io/hostname`.
+	// This annotation is added to Envoy Service, in order for ExternalDNS to create DNS entries.
+	// This solution is a workaround for projectcontour/contour#403
+	// More details regarding this workaround and other solutions is captured in
+	// https://github.com/kinvolk/PROJECT-Lokomotive-Kubernetes/issues/474
+	IngressHosts []string `hcl:"ingress_hosts,optional"`
 
-	// TODO: add num of replicas when using "deployment"
+	// IngressHostsRaw is not accessible to the user
+	IngressHostsRaw string
 }
 
 func newComponent() *component {
@@ -54,6 +67,14 @@ func (c *component) RenderManifests() (map[string]string, error) {
 			return nil, errors.Wrap(err, "failed to walk assets")
 		}
 	}
+	// To store the comma separated string representation of IngressHosts
+	c.IngressHostsRaw = strings.Join(c.IngressHosts, ",")
+	// Parse envoy service template.
+	envoyServiceStr, err := util.RenderTemplate(envoyServiceTmpl, c)
+	if err != nil {
+		return nil, errors.Wrap(err, "render template failed")
+	}
+	ret["02-service-envoy.yaml"] = envoyServiceStr
 
 	return ret, nil
 }
