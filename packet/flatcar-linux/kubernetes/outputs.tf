7cba19ee79763bc342cd1ba67b846f8f405f4630
Mauricio Vásquez <mauricio@kinvolk.io>
2020-01-24 11:04:20 -0500

    packet: fix DNS entries output
    Create a null_resource to force the name of the domains to be computed,
    otherwise they will be null on the output.
    
    Also fix the documentation for the user to get the list of entries from the
    null resource as the output from internal modules is nost saved on the state
    on Terraform 0.12.

--- packet/flatcar-linux/kubernetes/outputs.tf
+++ packet/flatcar-linux/kubernetes/outputs.tf
@@ -7,32 +7,5 @@ output "kubeconfig" {
 }
 
 output "dns_entries" {
-  value = concat(
-    # etcd
-    [
-      for device in packet_device.controllers:
-      {
-        name    = local.etcd_fqdn[index(packet_device.controllers, device)],
-        type    = "A",
-        ttl     = 300,
-        records = [device.access_private_ipv4],
-      }
-    ],
-    [
-      # apiserver public
-      {
-        name    = local.api_external_fqdn
-        type    = "A",
-        ttl     = 300,
-        records = packet_device.controllers.*.access_public_ipv4,
-      },
-      # apiserver private
-      {
-        name    = local.api_fqdn,
-        type    = "A",
-        ttl     = 300,
-        records = packet_device.controllers.*.access_private_ipv4,
-      },
-    ]
-  )
+  value = local.dns_entries
 }
