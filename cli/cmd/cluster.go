b01f23781250f1309a35fb8cb2f30e412de4e3b0
Mateusz Gozdek <mateusz@kinvolk.io>
2020-02-12 11:58:10 +0100

    cli/cmd: upgrade controlplane as part of install process
    This commit adds controlplane upgrade functionality to 'cluster install'
    command. If user runs this command on existing cluster, all controlplane
    helm releases will be upgraded using charts and values.yaml generated by
    Terraform.
    
    If some of controlplane release has been uninstalled by the user, it
    will be reinstalled to ensure consistency.
    
    The upgrade process is atomic, which means that if something goes wrong
    (e.g. new pod will not become ready), then the upgrade will be rolled
    back.
    
    The upgrade process is done in order recommended by upstream:
    https://kubernetes.io/docs/setup/release/version-skew-policy/#supported-component-upgrade-order
    
    Currently, 'kubelet' is not being upgraded, because of bug in runc on
    Flatcar Linux. See https://github.com/kinvolk/lokomotive/issues/110 for
    more details. For testing, --upgrade-kubelets can be used, but it's
    experimental and not by default at the moment.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- cli/cmd/cluster.go
+++ cli/cmd/cluster.go
@@ -15,12 +15,20 @@
 package cmd
 
 import (
+	"fmt"
+	"path/filepath"
+
 	"github.com/mitchellh/go-homedir"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/chart"
+	"helm.sh/helm/v3/pkg/chart/loader"
+	"sigs.k8s.io/yaml"
 
 	"github.com/kinvolk/lokomotive/pkg/backend"
 	"github.com/kinvolk/lokomotive/pkg/backend/local"
+	"github.com/kinvolk/lokomotive/pkg/components/util"
 	"github.com/kinvolk/lokomotive/pkg/config"
 	"github.com/kinvolk/lokomotive/pkg/platform"
 	"github.com/kinvolk/lokomotive/pkg/terraform"
@@ -137,4 +145,96 @@ func clusterExists(ctxLogger *logrus.Entry, ex *terraform.Executor) bool {
 	}
 
 	return len(o) != 0
+}
+
+type controlplaneUpdater struct {
+	kubeconfigPath string
+	assetDir       string
+	ctxLogger      logrus.Entry
+	ex             terraform.Executor
+}
+
+func (c controlplaneUpdater) getControlplaneChart(name string) (*chart.Chart, error) {
+	helmChart, err := loader.Load(filepath.Join(c.assetDir, "/lokomotive-kubernetes/bootkube/resources/charts", name))
+	if err != nil {
+		return nil, fmt.Errorf("loading chart from assets failed: %w", err)
+	}
+
+	if err := helmChart.Validate(); err != nil {
+		return nil, fmt.Errorf("chart is invalid: %w", err)
+	}
+
+	return helmChart, nil
+}
+
+func (c controlplaneUpdater) getControlplaneValues(name string) (map[string]interface{}, error) {
+	valuesRaw := ""
+	if err := c.ex.Output(fmt.Sprintf("%s_values", name), &valuesRaw); err != nil {
+		return nil, fmt.Errorf("failed to get controlplane component values.yaml from Terraform: %w", err)
+	}
+
+	values := map[string]interface{}{}
+	if err := yaml.Unmarshal([]byte(valuesRaw), &values); err != nil {
+		return nil, fmt.Errorf("failed to parse values.yaml for controlplane component: %w", err)
+	}
+
+	return values, nil
+}
+
+func (c controlplaneUpdater) upgradeComponent(component string) {
+	ctxLogger := c.ctxLogger.WithFields(logrus.Fields{
+		"action":    "controlplane-upgrade",
+		"component": component,
+	})
+
+	actionConfig, err := util.HelmActionConfig("kube-system", c.kubeconfigPath)
+	if err != nil {
+		ctxLogger.Fatalf("Failed initializing helm: %v", err)
+	}
+
+	helmChart, err := c.getControlplaneChart(component)
+	if err != nil {
+		ctxLogger.Fatalf("Loading chart from assets failed: %v", err)
+	}
+
+	values, err := c.getControlplaneValues(component)
+	if err != nil {
+		ctxLogger.Fatalf("Failed to get kubernetes values.yaml from Terraform: %v", err)
+	}
+
+	exists, err := util.ReleaseExists(*actionConfig, component)
+	if err != nil {
+		ctxLogger.Fatalf("Failed checking if controlplane component is installed: %v", err)
+	}
+
+	if !exists {
+		fmt.Printf("Controlplane component '%s' is missing, reinstalling...", component)
+
+		install := action.NewInstall(actionConfig)
+		install.ReleaseName = component
+		install.Namespace = "kube-system"
+		install.Atomic = true
+
+		if _, err := install.Run(helmChart, map[string]interface{}{}); err != nil {
+			fmt.Println("Failed!")
+
+			ctxLogger.Fatalf("Installing controlplane component failed: %v", err)
+		}
+
+		fmt.Println("Done.")
+	}
+
+	update := action.NewUpgrade(actionConfig)
+
+	update.Atomic = true
+
+	fmt.Printf("Ensuring controlplane component '%s' is up to date... ", component)
+
+	if _, err := update.Run(component, helmChart, values); err != nil {
+		fmt.Println("Failed!")
+
+		ctxLogger.Fatalf("Updating chart failed: %v", err)
+	}
+
+	fmt.Println("Done.")
 }
