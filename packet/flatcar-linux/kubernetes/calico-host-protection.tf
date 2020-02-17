91a7bc2e2a62092780e8b69306ceb6130f104403
Mateusz Gozdek <mateusz@kinvolk.io>
2020-02-17 11:08:13 +0100

    packet: move Calico host protection to helm chart
    To allow updating it, as right now if you try to upgrade Calico helm
    chart, it will remove GNPs.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

---
+++ packet/flatcar-linux/kubernetes/calico-host-protection.tf
@@ -0,0 +1,35 @@
+resource "local_file" "calico_host_protection" {
+  count = var.networking == "calico" ? 1 : 0
+
+  content  = templatefile("${path.module}/calico-host-protection.yaml.tmpl",{
+    host_endpoints = [
+      for device in packet_device.controllers :
+      {
+        name           = "${device.hostname}-bond0",
+        interface_name = "bond0",
+        labels         = {
+          "host-endpoint" = "ingress"
+          "nodetype"      = "controller"
+        }
+      }
+    ],
+    management_cidrs                = var.management_cidrs
+    cluster_cidrs                   = [
+      var.node_private_cidr,
+      var.pod_cidr,
+      var.service_cidr
+    ],
+  })
+
+  filename = "${var.asset_dir}/charts/kube-system/calico-host-protection.yaml"
+}
+
+# Populate calico-host-protection chart.
+# TODO: Currently, there is no way in Terraform to copy local directory, so we use `template_dir` for it.
+# The downside is, that any Terraform templating syntax stored in this directory will be evaluated, which may bring unexpected results.
+resource "template_dir" "calico_host_protection" {
+  count           = var.networking == "calico" ? 1 : 0
+
+  source_dir      = "${path.module}/calico-host-protection"
+  destination_dir = "${var.asset_dir}/charts/kube-system/calico-host-protection"
+}
