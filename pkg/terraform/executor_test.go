c9ef6ae045d9216874a0d6a825117331c96c6cf3
Mateusz Gozdek <mateusz@kinvolk.io>
2020-03-06 15:27:02 +0100

    pkg/terraform/executor.go: include key in Output error message
    So it's easier to debug which key is wrong.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- pkg/terraform/executor_test.go
+++ pkg/terraform/executor_test.go
@@ -5,10 +5,11 @@ package terraform
 import (
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 )
 
-func TestExecuteCheckErrors(t *testing.T) {
+func executor(t *testing.T) *Executor {
 	tmpDir, err := ioutil.TempDir("", "lokoctl-tests-")
 	if err != nil {
 		t.Fatalf("Creating tmp dir should succeed, got: %v", err)
@@ -26,7 +27,29 @@ func TestExecuteCheckErrors(t *testing.T) {
 		t.Fatalf("Creating new executor should succeed, got: %v", err)
 	}
 
+	return ex
+}
+
+func TestExecuteCheckErrors(t *testing.T) {
+	ex := executor(t)
+
 	if err := ex.Execute("apply"); err == nil {
 		t.Fatalf("Applying on empty directory should fail")
+	}
+}
+
+func TestOutputIncludeKeyInError(t *testing.T) {
+	ex := executor(t)
+
+	k := "foo"
+	o := ""
+
+	err := ex.Output(k, &o)
+	if err == nil {
+		t.Fatalf("Output should fail on non existing installation")
+	}
+
+	if !strings.Contains(err.Error(), k) {
+		t.Fatalf("Error message should contain key, got: %v", err)
 	}
 }
