kinvolk/lokomotive

commit 485994b8baef25a8b2abeb2731fd659e6c35a34c

*: lokoctl cluster install -> lokoctl cluster apply
This aligns more with the idempotency of this operation.

We also rename the relevant package and functions in the code.

cli/cmd/cluster-apply.go
@@ -32,24 +32,25 @@ var (
 	upgradeKubelets bool
 )
 
-var clusterInstallCmd = &cobra.Command{
-	Use:   "install",
-	Short: "Install Lokomotive cluster with components",
-	Run:   runClusterInstall,
+var clusterApplyCmd = &cobra.Command{
+	Use:   "apply",
+	Short: "Apply configuration changes to a Lokomotive cluster with components",
+	Run:   runClusterApply,
 }
 
 func init() {
-	clusterCmd.AddCommand(clusterInstallCmd)
-	pf := clusterInstallCmd.PersistentFlags()
+	clusterCmd.AddCommand(clusterApplyCmd)
+	pf := clusterApplyCmd.PersistentFlags()
 	pf.BoolVarP(&confirm, "confirm", "", false, "Upgrade cluster without asking for confirmation")
 	pf.BoolVarP(&verbose, "verbose", "v", false, "Show output from Terraform")
-	pf.BoolVarP(&skipComponents, "skip-components", "", false, "Skip component installation")
+	pf.BoolVarP(&skipComponents, "skip-components", "", false, "Skip applying component configuration")
 	pf.BoolVarP(&upgradeKubelets, "upgrade-kubelets", "", false, "Experimentally upgrade self-hosted kubelets")
 }
 
-func runClusterInstall(cmd *cobra.Command, args []string) {
+//nolint:funlen
+func runClusterApply(cmd *cobra.Command, args []string) {
 	ctxLogger := log.WithFields(log.Fields{
-		"command": "lokoctl cluster install",
+		"command": "lokoctl cluster apply",
 		"args":    args,
 	})
 
@@ -62,22 +63,22 @@ func runClusterInstall(cmd *cobra.Command, args []string) {
 			ctxLogger.Fatalf("Failed to reconsile cluster state: %v", err)
 		}
 
-		if !askForConfirmation("Do you want to proceed with cluster install?") {
-			ctxLogger.Println("Cluster install cancelled")
+		if !askForConfirmation("Do you want to proceed with cluster apply?") {
+			ctxLogger.Println("Cluster apply cancelled")
 
 			return
 		}
 	}
 
-	if err := p.Install(ex); err != nil {
-		ctxLogger.Fatalf("error installing cluster: %v", err)
+	if err := p.Apply(ex); err != nil {
+		ctxLogger.Fatalf("error applying cluster: %v", err)
 	}
 
 	fmt.Printf("\nYour configurations are stored in %s\n", assetDir)
 
 	kubeconfigPath := assetsKubeconfig(assetDir)
-	if err := verifyInstall(kubeconfigPath, p.GetExpectedNodes()); err != nil {
-		ctxLogger.Fatalf("Verify cluster installation: %v", err)
+	if err := verifyCluster(kubeconfigPath, p.GetExpectedNodes()); err != nil {
+		ctxLogger.Fatalf("Verify cluster: %v", err)
 	}
 
 	// Do controlplane upgrades only if cluster already exists.
@@ -106,21 +107,21 @@ func runClusterInstall(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	var componentsToInstall []string
+	componentsToApply := make([]string, len(lokoConfig.RootConfig.Components))
 	for _, component := range lokoConfig.RootConfig.Components {
-		componentsToInstall = append(componentsToInstall, component.Name)
+		componentsToApply = append(componentsToApply, component.Name)
 	}
 
-	ctxLogger.Println("Installing components")
+	ctxLogger.Println("Applying component configuration")
 
-	if len(componentsToInstall) > 0 {
-		if err := installComponents(lokoConfig, kubeconfigPath, componentsToInstall...); err != nil {
-			ctxLogger.Fatalf("Installing components failed: %v", err)
+	if len(componentsToApply) > 0 {
+		if err := applyComponents(lokoConfig, kubeconfigPath, componentsToApply...); err != nil {
+			ctxLogger.Fatalf("Applying component configuration failed: %v", err)
 		}
 	}
 }
 
-func verifyInstall(kubeconfigPath string, expectedNodes int) error {
+func verifyCluster(kubeconfigPath string, expectedNodes int) error {
 	client, err := k8sutil.NewClientset(kubeconfigPath)
 	if err != nil {
 		return errors.Wrapf(err, "failed to set up clientset")

pkg/platform/aws/aws.go
@@ -92,7 +92,7 @@ func (c *config) GetAssetDir() string {
 	return c.AssetDir
 }
 
-func (c *config) Install(ex *terraform.Executor) error {
+func (c *config) Apply(ex *terraform.Executor) error {
 	if err := c.Initialize(ex); err != nil {
 		return err
 	}

pkg/platform/baremetal/baremetal.go
@@ -75,7 +75,7 @@ func NewConfig() *config {
 	}
 }
 
-func (c *config) Install(ex *terraform.Executor) error {
+func (c *config) Apply(ex *terraform.Executor) error {
 	if err := c.Initialize(ex); err != nil {
 		return err
 	}

pkg/platform/packet/packet.go
@@ -130,7 +130,7 @@ func (c *config) Initialize(ex *terraform.Executor) error {
 	return createTerraformConfigFile(c, terraformRootDir)
 }
 
-func (c *config) Install(ex *terraform.Executor) error {
+func (c *config) Apply(ex *terraform.Executor) error {
 	assetDir, err := homedir.Expand(c.AssetDir)
 	if err != nil {
 		return err

pkg/platform/platform.go
@@ -24,7 +24,7 @@ import (
 // Platform describes single environment, where cluster can be installed
 type Platform interface {
 	LoadConfig(*hcl.Body, *hcl.EvalContext) hcl.Diagnostics
-	Install(*terraform.Executor) error
+	Apply(*terraform.Executor) error
 	Destroy(*terraform.Executor) error
 	Initialize(*terraform.Executor) error
 	GetAssetDir() string

pkg/terraform/util.go
@@ -16,10 +16,11 @@ package terraform
 
 import (
 	"fmt"
-	"github.com/kinvolk/lokomotive/pkg/install"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/kinvolk/lokomotive/pkg/install"
 
 	"github.com/pkg/errors"
 )

commit 1615a3becc77f247e9ba3afc8a995564da813504

cli/cmd/cluster-apply.go: initialize components to apply as empty slice
This commit fixes a regression introduced in
485994b8baef25a8b2abeb2731fd659e6c35a34c, which causes, that 'cluster
apply' command can no longer install components.

This is happening, because it changed the way we initialize the
componentsToApply variable. Before it was a nil slice and mentioned
commit changed it to initialize it as an slice with number of empty
elements equal to number of configured components. As we use 'append'
later, we end up with N empty elements and N valid elements, hence the
components installation fails.

This commit changes an initialation to simply assignment, which is a
most common approach for working with slices in Go, together with
'append'. We don't need to use 'make' for optimization here.

Closes #160

Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

cli/cmd/cluster-apply.go
@@ -107,7 +107,7 @@ func runClusterApply(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	componentsToApply := make([]string, len(lokoConfig.RootConfig.Components))
+	componentsToApply := []string{}
 	for _, component := range lokoConfig.RootConfig.Components {
 		componentsToApply = append(componentsToApply, component.Name)
 	}