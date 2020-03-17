b5edcb9edfa1965bb54c6aae0f5cb00db67ad08e
Mateusz Gozdek <mateusz@kinvolk.io>
2020-03-17 19:01:11 +0100

    test/monitoring/control-plane_test.go: enable on AWS
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- test/monitoring/control-plane_test.go
+++ test/monitoring/control-plane_test.go
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO: Add AWS when prometheus operator is being installed on AWS
-// +build packet
+// +build aws packet
 // +build poste2e
 
 package monitoring
