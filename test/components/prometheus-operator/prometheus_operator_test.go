71c40a95e80c4d698be5feefc767be423fa42cd6
Mateusz Gozdek <mateusz@kinvolk.io>
2020-03-17 16:28:03 +0100

    test/components/prometheus-operator: enable testing on AWS
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- test/components/prometheus-operator/prometheus_operator_test.go
+++ test/components/prometheus-operator/prometheus_operator_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build packet
+// +build aws packet
 // +build e2e
 
 package prometheusoperator
