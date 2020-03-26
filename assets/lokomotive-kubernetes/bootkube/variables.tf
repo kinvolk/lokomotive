4585975f441a4edffb99b7eb7e58fbfcfa524b13
Suraj Deshmukh <suraj@kinvolk.io>
2020-03-26 18:11:50 +0530

    controller-manager: Move to use dedicated image
    This commit changes the controller-manager configs to use the dedicated
    controller-manager container image instead of using hyperkube image.
    
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- assets/lokomotive-kubernetes/bootkube/variables.tf
+++ assets/lokomotive-kubernetes/bootkube/variables.tf
@@ -89,12 +89,13 @@ variable "container_images" {
   type        = map(string)
 
   default = {
-    calico           = "calico/node:v3.13.1"
-    calico_cni       = "calico/cni:v3.13.1"
-    hyperkube        = "k8s.gcr.io/hyperkube:v1.18.0"
-    coredns          = "coredns/coredns:coredns-"
-    pod_checkpointer = "kinvolk/pod-checkpointer:83e25e5968391b9eb342042c435d1b3eeddb2be1"
-    kube_apiserver   = "k8s.gcr.io/kube-apiserver:v1.18.0"
+    calico                  = "calico/node:v3.13.1"
+    calico_cni              = "calico/cni:v3.13.1"
+    hyperkube               = "k8s.gcr.io/hyperkube:v1.18.0"
+    coredns                 = "coredns/coredns:coredns-"
+    pod_checkpointer        = "kinvolk/pod-checkpointer:83e25e5968391b9eb342042c435d1b3eeddb2be1"
+    kube_apiserver          = "k8s.gcr.io/kube-apiserver:v1.18.0"
+    kube_controller_manager = "k8s.gcr.io/kube-controller-manager:v1.18.0"
   }
 }
 
