39dd779a8cbfb81506a9f698722b63114d04392d
Suraj Deshmukh <suraj@kinvolk.io>
2020-04-02 17:23:54 +0530

    AWS: Add variable `expose_nodeports`
    This boolean type variable is used to expose node ports in the security
    group of AWS installation. By default this variable is set to `false`.
    
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- assets/lokomotive-kubernetes/aws/flatcar-linux/kubernetes/security.tf
+++ assets/lokomotive-kubernetes/aws/flatcar-linux/kubernetes/security.tf
@@ -335,3 +335,15 @@ resource "aws_security_group_rule" "worker-egress" {
   cidr_blocks      = ["0.0.0.0/0"]
   ipv6_cidr_blocks = ["::/0"]
 }
+
+resource "aws_security_group_rule" "worker-nodeport" {
+  count = var.expose_nodeports ? 1 : 0
+
+  security_group_id = aws_security_group.worker.id
+
+  type      = "ingress"
+  protocol  = "tcp"
+  from_port = 30000
+  to_port   = 32767
+  self      = true
+}
