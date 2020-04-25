2e73fea8a3b0b9832bd8f0123a88e0d307796ee0
Imran Pochi <imran@kinvolk.io>
2020-04-25 13:50:14 +0530

    Remove not needed functions from cmd/cluster.go
    - removing the functions that are not needed anymore as their
      implmentations are ported to pkg/lokomotive package.
    
    Signed-off-by: Imran Pochi <imran@kinvolk.io>

--- cli/cmd/cluster.go
+++ cli/cmd/cluster.go
@@ -15,23 +15,13 @@
 package cmd
 
 import (
-	"fmt"
-	"path/filepath"
-
-	"github.com/mitchellh/go-homedir"
-	"github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
-	"helm.sh/helm/v3/pkg/action"
-	"helm.sh/helm/v3/pkg/chart"
-	"helm.sh/helm/v3/pkg/chart/loader"
-	"sigs.k8s.io/yaml"
-
-	"github.com/kinvolk/lokomotive/pkg/backend"
 	"github.com/kinvolk/lokomotive/pkg/backend/local"
-	"github.com/kinvolk/lokomotive/pkg/components/util"
 	"github.com/kinvolk/lokomotive/pkg/config"
 	"github.com/kinvolk/lokomotive/pkg/platform"
 	"github.com/kinvolk/lokomotive/pkg/terraform"
+	"github.com/mitchellh/go-homedir"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 )
 
 var clusterCmd = &cobra.Command{
@@ -92,149 +82,4 @@ func initialize(ctxLogger *logrus.Entry) (*terraform.Executor, platform.Platform
 	ex := initializeTerraform(ctxLogger, p, b)
 
 	return ex, p, lokoConfig, assetDir
-}
-
-// initializeTerraform initialized Terraform directory using given backend and platform
-// and returns configured executor.
-func initializeTerraform(ctxLogger *logrus.Entry, p platform.Platform, b backend.Backend) *terraform.Executor {
-	assetDir, err := homedir.Expand(p.GetAssetDir())
-	if err != nil {
-		ctxLogger.Fatalf("Error expanding path: %v", err)
-	}
-
-	// Render backend configuration.
-	renderedBackend, err := b.Render()
-	if err != nil {
-		ctxLogger.Fatalf("Failed to render backend configuration file: %v", err)
-	}
-
-	// Configure Terraform directory, module and backend.
-	if err := terraform.Configure(assetDir, renderedBackend); err != nil {
-		ctxLogger.Fatalf("Failed to configure Terraform : %v", err)
-	}
-
-	conf := terraform.Config{
-		WorkingDir: terraform.GetTerraformRootDir(assetDir),
-		Verbose:    verbose,
-	}
-
-	ex, err := terraform.NewExecutor(conf)
-	if err != nil {
-		ctxLogger.Fatalf("Failed to create Terraform executor: %v", err)
-	}
-
-	if err := p.Initialize(ex); err != nil {
-		ctxLogger.Fatalf("Failed to initialize Platform: %v", err)
-	}
-
-	if err := ex.Init(); err != nil {
-		ctxLogger.Fatalf("Failed to initialize Terraform: %v", err)
-	}
-
-	return ex
-}
-
-// clusterExists determines if cluster has already been created by getting all
-// outputs from the Terraform. If there is any output defined, it means 'terraform apply'
-// run at least once.
-func clusterExists(ctxLogger *logrus.Entry, ex *terraform.Executor) bool {
-	o := map[string]interface{}{}
-
-	if err := ex.Output("", &o); err != nil {
-		ctxLogger.Fatalf("Failed to check if cluster exists: %v", err)
-	}
-
-	return len(o) != 0
-}
-
-type controlplaneUpdater struct {
-	kubeconfigPath string
-	assetDir       string
-	ctxLogger      logrus.Entry
-	ex             terraform.Executor
-}
-
-func (c controlplaneUpdater) getControlplaneChart(name string) (*chart.Chart, error) {
-	helmChart, err := loader.Load(filepath.Join(c.assetDir, "/lokomotive-kubernetes/bootkube/resources/charts", name))
-	if err != nil {
-		return nil, fmt.Errorf("loading chart from assets failed: %w", err)
-	}
-
-	if err := helmChart.Validate(); err != nil {
-		return nil, fmt.Errorf("chart is invalid: %w", err)
-	}
-
-	return helmChart, nil
-}
-
-func (c controlplaneUpdater) getControlplaneValues(name string) (map[string]interface{}, error) {
-	valuesRaw := ""
-	if err := c.ex.Output(fmt.Sprintf("%s_values", name), &valuesRaw); err != nil {
-		return nil, fmt.Errorf("failed to get controlplane component values.yaml from Terraform: %w", err)
-	}
-
-	values := map[string]interface{}{}
-	if err := yaml.Unmarshal([]byte(valuesRaw), &values); err != nil {
-		return nil, fmt.Errorf("failed to parse values.yaml for controlplane component: %w", err)
-	}
-
-	return values, nil
-}
-
-func (c controlplaneUpdater) upgradeComponent(component string) {
-	ctxLogger := c.ctxLogger.WithFields(logrus.Fields{
-		"action":    "controlplane-upgrade",
-		"component": component,
-	})
-
-	actionConfig, err := util.HelmActionConfig("kube-system", c.kubeconfigPath)
-	if err != nil {
-		ctxLogger.Fatalf("Failed initializing helm: %v", err)
-	}
-
-	helmChart, err := c.getControlplaneChart(component)
-	if err != nil {
-		ctxLogger.Fatalf("Loading chart from assets failed: %v", err)
-	}
-
-	values, err := c.getControlplaneValues(component)
-	if err != nil {
-		ctxLogger.Fatalf("Failed to get kubernetes values.yaml from Terraform: %v", err)
-	}
-
-	exists, err := util.ReleaseExists(*actionConfig, component)
-	if err != nil {
-		ctxLogger.Fatalf("Failed checking if controlplane component is installed: %v", err)
-	}
-
-	if !exists {
-		fmt.Printf("Controlplane component '%s' is missing, reinstalling...", component)
-
-		install := action.NewInstall(actionConfig)
-		install.ReleaseName = component
-		install.Namespace = "kube-system"
-		install.Atomic = true
-
-		if _, err := install.Run(helmChart, map[string]interface{}{}); err != nil {
-			fmt.Println("Failed!")
-
-			ctxLogger.Fatalf("Installing controlplane component failed: %v", err)
-		}
-
-		fmt.Println("Done.")
-	}
-
-	update := action.NewUpgrade(actionConfig)
-
-	update.Atomic = true
-
-	fmt.Printf("Ensuring controlplane component '%s' is up to date... ", component)
-
-	if _, err := update.Run(component, helmChart, values); err != nil {
-		fmt.Println("Failed!")
-
-		ctxLogger.Fatalf("Updating chart failed: %v", err)
-	}
-
-	fmt.Println("Done.")
 }
