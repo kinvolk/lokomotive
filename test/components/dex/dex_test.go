7e2386369348beccdeedd643d8fe5b4053464db9
Iago López Galeiras <iago@kinvolk.io>
2020-05-06 12:36:26 +0200

    ci: rename aws-edge to aws_edge
    Go build constraints don't allow dashes, only underscores[1].
    
    [1]: https://golang.org/pkg/go/build/#hdr-Build_Constraints

--- test/components/dex/dex_test.go
+++ test/components/dex/dex_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build aws aws-edge packet
+// +build aws aws_edge packet
 // +build e2e
 
 package dex
