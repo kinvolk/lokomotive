f4cc825fe932286ecd71ed2f6ea3a0b2c2fb8238
Iago López Galeiras <iago@kinvolk.io>
2020-05-06 11:46:59 +0200

    test: run tests on AWS with Flatcar edge too
    There were no tests being run with Flatcar edge.

--- test/components/kubernetes/kubelet_test.go
+++ test/components/kubernetes/kubelet_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build aws packet
+// +build aws aws-edge packet
 // +build e2e
 
 package kubernetes
