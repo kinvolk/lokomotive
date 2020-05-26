c9cd1cf57a5a8cef3f6ea45bfd159adcad7b6411
Imran Pochi <imran@kinvolk.io>
2020-05-26 17:55:01 +0530

    terraform: signal handler for OS interrupts
    - Fix issue #329, to handle OS interrupts.
    
    - signalHandler returns an instance of handler, currently we only
      deal with the signal os.Interrupt.
    
    - stop terminates listening of interrupts once the terraform process has
      released its resources by either successful exit for forced
      termination. This way there are no leaking terraform processes.
    
    Signed-off-by: Imran Pochi <imran@kinvolk.io>

---
+++ pkg/terraform/signals.go
@@ -0,0 +1,60 @@
+// Copyright 2020 The Lokomotive Authors
+// Copyright 2017 CoreOS, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package terraform
+
+import (
+	"os"
+	"os/signal"
+
+	"github.com/sirupsen/logrus"
+)
+
+// handler defines signal handler.
+type handler struct {
+	logger *logrus.Entry
+	// signalCh stores signals received from the system.
+	signalCh chan os.Signal
+}
+
+// signalHandler creates a new Handler.
+func signalHandler(logger *logrus.Entry) *handler {
+	// Different signals that we are listening to. For the time being putting
+	// only os.Interrupt, making a slice so that we can easily add more.
+	signals := []os.Signal{os.Interrupt}
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, signals...)
+
+	// Goroutine that prints the message to the user in case the --verbose
+	// option is not used.
+	go func() {
+		for range signalCh {
+			logger.Warnln("Interrupt received, please wait for Terraform to terminate.")
+		}
+	}()
+
+	return &handler{
+		signalCh: signalCh,
+		logger:   logger,
+	}
+}
+
+func (h *handler) stop() {
+	// Stop listening for interrupts on the channel
+	signal.Stop(h.signalCh)
+	// Close the channel.
+	close(h.signalCh)
+}
