6a2c4a85b1e68a155d6af76fe62444893235f4fc
Mateusz Gozdek <mateusz@kinvolk.io>
2020-05-29 15:23:12 +0200

    test/components/kubernetes: add tests that nodes has configured labels
    Now that we don't run disruptive test for self-hosted kubelet, we should
    have a test, that the labels configured by the user for the worker pool
    are actually applied on the node objects.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

---
+++ test/components/kubernetes/doc.go
@@ -0,0 +1,20 @@
+// Copyright 2020 The Lokomotive Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build aws aws_edge packet baremetal
+// +build e2e
+
+// Package kubernetes contains e2e tests related to core Kubernetes
+// services, like kubelet etc.
+package kubernetes
