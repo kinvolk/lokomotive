019b2d86c5485746ee9361d9fb550e14a5581043
Suraj Deshmukh <suraj@kinvolk.io>
2020-06-30 14:17:44 +0530

    components: Add istio-operator
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- cli/cmd/component.go
+++ cli/cmd/component.go
@@ -27,6 +27,7 @@ import (
 	_ "github.com/kinvolk/lokomotive/pkg/components/flatcar-linux-update-operator"
 	_ "github.com/kinvolk/lokomotive/pkg/components/gangway"
 	_ "github.com/kinvolk/lokomotive/pkg/components/httpbin"
+	_ "github.com/kinvolk/lokomotive/pkg/components/istio-operator"
 	_ "github.com/kinvolk/lokomotive/pkg/components/metallb"
 	_ "github.com/kinvolk/lokomotive/pkg/components/metrics-server"
 	_ "github.com/kinvolk/lokomotive/pkg/components/openebs-operator"
