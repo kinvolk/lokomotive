2df5d18235aaa8736bffd426029be95c35de13c6
Suraj Deshmukh <suraj@kinvolk.io>
2020-07-07 14:10:21 +0530

    linkerd: Add component code
    - Use cert creation API from linkerd.
    
    - Vendor `linkerd` libraries needed for cert generation.
    
    - Add content from `values-ha.yaml` file to the `manifest.go` to add
    support for HA installation. In the HA mode user can specify the number
    of controllers that should be created using a variable
    `controller_replicas`.
    
    **Caution**: If the number of linkerd controller count is more than the
    available worker nodes then some pods may stay in `Pending` state.
    
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

---
+++ pkg/components/linkerd/component_test.go
@@ -0,0 +1,70 @@
+// Copyright 2020 The Lokomotive Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// nolint:testpackage
+package linkerd
+
+import "testing"
+
+func Test_indent(t *testing.T) {
+	type args struct {
+		data   string
+		indent int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			args: args{
+				data: `foo:
+  bar:
+  - baz`,
+				indent: 2,
+			},
+			want: `  foo:
+    bar:
+    - baz`,
+		},
+		{
+			args: args{
+				data:   "singleline",
+				indent: 3,
+			},
+			want: "   singleline",
+		},
+		{
+			args: args{
+				data: `a:
+  b: foobar
+`,
+				indent: 4,
+			},
+			want: `    a:
+      b: foobar
+`,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := indent(tt.args.data, tt.args.indent); got != tt.want {
+				t.Errorf("indent() = \n%v\nwant =\n%v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ pkg/components/linkerd/manifest.go
@@ -0,0 +1,108 @@
+// Copyright 2020 The Lokomotive Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package linkerd has code related to deployment of istio operator component.
+package linkerd
+
+const chartValuesTmpl = `
+global:
+  identityTrustAnchorsPEM: |
+{{ .Cert.CA }}
+
+identity:
+  issuer:
+    crtExpiry: {{ .Cert.Expiry }}
+    tls:
+      crtPEM: |
+{{ .Cert.Cert }}
+      keyPEM: |
+{{ .Cert.Key }}
+
+# controller configuration
+controllerReplicas: {{.ControllerReplicas}}
+`
+
+// Contents of the values-ha.yaml file are copied here verbatim. Necessary fields are overridden in
+// `chartValuesTmpl` using user provided information.
+const valuesHA = `
+# This values.yaml file contains the values needed to enable HA mode.
+# Usage:
+#   helm install -f values.yaml -f values-ha.yaml
+
+enablePodAntiAffinity: true
+
+global:
+  # proxy configuration
+  proxy:
+    resources:
+      cpu:
+        limit: "1"
+        request: 100m
+      memory:
+        limit: 250Mi
+        request: 20Mi
+
+# controller configuration
+controllerReplicas: 3
+controllerResources: &controller_resources
+  cpu: &controller_resources_cpu
+    limit: "1"
+    request: 100m
+  memory:
+    limit: 250Mi
+    request: 50Mi
+destinationResources: *controller_resources
+publicAPIResources: *controller_resources
+
+# identity configuration
+identityResources:
+  cpu: *controller_resources_cpu
+  memory:
+    limit: 250Mi
+    request: 10Mi
+
+# grafana configuration
+grafana:
+  resources:
+    cpu: *controller_resources_cpu
+    memory:
+      limit: 1024Mi
+      request: 50Mi
+
+# heartbeat configuration
+heartbeatResources: *controller_resources
+
+# prometheus configuration
+prometheusResources:
+  cpu:
+    limit: "4"
+    request: 300m
+  memory:
+    limit: 8192Mi
+    request: 300Mi
+
+# proxy injector configuration
+proxyInjectorResources: *controller_resources
+
+webhookFailurePolicy: Fail
+
+# service profile validator configuration
+spValidatorResources: *controller_resources
+
+# tap configuration
+tapResources: *controller_resources
+
+# web configuration
+webResources: *controller_resources
+`
