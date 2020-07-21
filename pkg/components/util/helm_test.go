1ee645590f274bd80ba42e91a26f8e21e2381980
Mateusz Gozdek <mateusz@kinvolk.io>
2020-07-21 23:50:12 +0200

    pkg/components/util: use Helm post-renderer instead of Manifests field
    As Manifests filed in Chart object is a custom field, added to our fork
    of Helm. Now Helm offers post-renderer feature, which allows to further
    customize the manfiests after Helm's templating engine and before the
    installation of the manifests.
    
    This allows us to use post-renderer feature and drop one of maintained
    custom patches, which means less maintenance for us.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- pkg/components/util/helm_test.go
+++ pkg/components/util/helm_test.go
@@ -106,11 +106,11 @@ metadata:
 		t.Fatalf("Chart should be created, got: %v", err)
 	}
 
-	if len(chart.Manifests) != 1 { //nolint:gomnd
+	if len(chart.Templates) != 1 {
 		t.Fatalf("Manifest file with the namespace should still be added, as it may contain other objects")
 	}
 
-	if len(chart.Manifests[0].Data) != 0 {
+	if len(chart.Templates[0].Data) != 0 {
 		t.Fatalf("Namespace object should be removed from chart")
 	}
 }
@@ -140,7 +140,11 @@ metadata:
 		t.Fatalf("Chart should be created, got: %v", err)
 	}
 
-	if len(chart.Manifests[0].Data) == 0 {
+	if len(chart.Templates) != 1 {
+		t.Fatalf("templates should include exactly one object")
+	}
+
+	if len(chart.Templates[0].Data) == 0 {
 		t.Fatalf("Other objects should be retained in the file containing Namespace object")
 	}
 }
@@ -165,7 +169,7 @@ metadata:
 		t.Fatalf("Chart should be created, got: %v", err)
 	}
 
-	if len(chart.Manifests[0].Data) == 0 {
+	if len(chart.Templates[0].Data) == 0 {
 		t.Fatalf("Only Namespace object with matching namespace name should be filtered")
 	}
 }
@@ -188,15 +192,15 @@ metadata:
 		t.Fatalf("Chart should be created, got: %v", err)
 	}
 
-	if len(chart.Manifests) != 1 { //nolint:gomnd
+	if len(chart.Templates) != 1 {
 		t.Fatalf("Manifest file with the CRDs should still be added, as it may contain other objects")
 	}
 
-	if len(chart.Manifests[0].Data) != 0 {
+	if len(chart.Templates[0].Data) != 0 {
 		t.Fatalf("CRD object should be removed from the manifests file")
 	}
 
-	if len(chart.Files) != 1 { //nolint:gomnd
+	if len(chart.Files) != 1 {
 		t.Fatalf("CRD object should be added to Files field")
 	}
 
