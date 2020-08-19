57f8ab7aadd188afc4f59f053ca544b927de8675
Imran Pochi <imran@kinvolk.io>
2020-08-19 16:43:06 +0530

    refactor: add new method to interface to get provider specific values
    This commit moves the helm values.yaml template to a separate file
    `template.go` for each provider.
    
    In this case, since the only provider is azure, it is moved to
    `azure/template.go`.
    
    This allows for multiple templates, each specific to the provider as
    each provider will have provider specific BackupStorageLocation and
    VolumeSnapshotLocation.
    
    Values method returns the values specific to the plugin provider. This
    is then clubbed together with the common values for the chart such as
    metrics for rendering the chart.
    
    Signed-off-by: Imran Pochi <imran@kinvolk.io>

---
+++ pkg/components/velero/azure/template.go
@@ -0,0 +1,52 @@
+// Copyright 2020 The Lokomotive Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package azure
+
+const chartValuesTmpl = `
+configuration:
+  provider: azure
+  backupStorageLocation:
+    name: azure
+    provider: velero.io/azure
+    bucket: {{ .BackupStorageLocation.Bucket }}
+    config:
+      resourceGroup: {{ .BackupStorageLocation.ResourceGroup }}
+      storageAccount: {{ .BackupStorageLocation.StorageAccount }}
+  volumeSnapshotLocation:
+    name: azure
+    config:
+      {{- if .VolumeSnapshotLocation.ResourceGroup }}
+      resourceGroup: {{ .VolumeSnapshotLocation.ResourceGroup }}
+      {{- end }}
+      apitimeout: {{ .VolumeSnapshotLocation.APITimeout }}
+credentials:
+  secretContents:
+    cloud: |
+      AZURE_SUBSCRIPTION_ID: "{{ .SubscriptionID }}"
+      AZURE_TENANT_ID: "{{ .TenantID }}"
+      AZURE_CLIENT_ID: "{{ .ClientID }}"
+      AZURE_CLIENT_SECRET: "{{ .ClientSecret }}"
+      AZURE_RESOURCE_GROUP: "{{ .ResourceGroup }}"
+initContainers:
+- image: velero/velero-plugin-for-microsoft-azure:v1.0.0
+  imagePullPolicy: IfNotPresent
+  name: velero-plugin-for-azure
+  resources: {}
+  terminationMessagePath: /dev/termination-log
+  terminationMessagePolicy: File
+  volumeMounts:
+  - mountPath: /target
+    name: plugins
+`
--- pkg/components/velero/component.go
+++ pkg/components/velero/component.go
@@ -56,6 +56,7 @@ type Metrics struct {
 
 // Provider requires implementing config validation function for each provider
 type provider interface {
+	Values() (string, error)
 	Validate() hcl.Diagnostics
 }
 
@@ -73,45 +74,12 @@ func newComponent() *component {
 }
 
 const chartValuesTmpl = `
-configuration:
-  provider: {{ .Provider }}
-  backupStorageLocation:
-    name: {{ .Provider }}
-    bucket: {{ .Azure.BackupStorageLocation.Bucket }}
-    config:
-      resourceGroup: {{ .Azure.BackupStorageLocation.ResourceGroup }}
-      storageAccount: {{ .Azure.BackupStorageLocation.StorageAccount }}
-  volumeSnapshotLocation:
-    name: {{ .Provider }}
-    config:
-      {{- if .Azure.VolumeSnapshotLocation.ResourceGroup }}
-      resourceGroup: {{ .Azure.VolumeSnapshotLocation.ResourceGroup }}
-      {{- end }}
-      apitimeout: {{ .Azure.VolumeSnapshotLocation.APITimeout }}
-credentials:
-  secretContents:
-    cloud: |
-      AZURE_SUBSCRIPTION_ID: "{{ .Azure.SubscriptionID }}"
-      AZURE_TENANT_ID: "{{ .Azure.TenantID }}"
-      AZURE_CLIENT_ID: "{{ .Azure.ClientID }}"
-      AZURE_CLIENT_SECRET: "{{ .Azure.ClientSecret }}"
-      AZURE_RESOURCE_GROUP: "{{ .Azure.ResourceGroup }}"
 metrics:
   enabled: {{ .Metrics.Enabled }}
   serviceMonitor:
     enabled: {{ .Metrics.ServiceMonitor }}
     additionalLabels:
       release: prometheus-operator
-initContainers:
-- image: velero/velero-plugin-for-microsoft-azure:v1.0.0
-  imagePullPolicy: IfNotPresent
-  name: velero-plugin-for-azure
-  resources: {}
-  terminationMessagePath: /dev/termination-log
-  terminationMessagePolicy: File
-  volumeMounts:
-  - mountPath: /target
-    name: plugins
 `
 
 // LoadConfig decodes given HCL and validates the configuration.
@@ -154,9 +122,9 @@ func (c *component) RenderManifests() (map[string]string, error) {
 		return nil, fmt.Errorf("retrieving chart from assets: %w", err)
 	}
 
-	values, err := template.Render(chartValuesTmpl, c)
+	values, err := c.values()
 	if err != nil {
-		return nil, fmt.Errorf("rendering chart values template: %w", err)
+		return nil, fmt.Errorf("getting values: %w", err)
 	}
 
 	renderedFiles, err := util.RenderChart(helmChart, name, c.Namespace, values)
@@ -165,6 +133,27 @@ func (c *component) RenderManifests() (map[string]string, error) {
 	}
 
 	return renderedFiles, nil
+}
+
+// values renders common values for all providers, provider specific values and
+// concatenates them.
+func (c *component) values() (string, error) {
+	commonValues, err := template.Render(chartValuesTmpl, c)
+	if err != nil {
+		return "", fmt.Errorf("rendering common values template: %w", err)
+	}
+
+	p, err := c.getProvider()
+	if err != nil {
+		return "", fmt.Errorf("getting provider: %w", err)
+	}
+
+	values, err := p.Values()
+	if err != nil {
+		return "", fmt.Errorf("getting provider values: %w", err)
+	}
+
+	return commonValues + values, nil
 }
 
 // validate validates component configuration
