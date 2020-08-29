dff83a724ca00d385aeaea2eefa6dd888dd0594d
Imran Pochi <imran@kinvolk.io>
2020-08-29 22:16:28 +0530

    velero: add package level comment for azure
    Signed-off-by: Imran Pochi <imran@kinvolk.io>

--- pkg/components/velero/azure/azure.go
+++ pkg/components/velero/azure/azure.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package azure deals with configuring Velero azure plugin.
 package azure
 
 import (
