3b306b4c4c2fb8aaf97093d2d3b05a09f23f481d
Mateusz Gozdek <mateusz@kinvolk.io>
2020-09-11 13:45:03 +0200

    pkg/install: remove Verify to pkg/lokomotive package
    As it only takes Cluster object as an argument, it make sense to make it
    a method of Cluster type.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- pkg/lokomotive/cluster.go
+++ pkg/lokomotive/cluster.go
@@ -20,10 +20,24 @@ import (
 	"os"
 	"strings"
 	"text/tabwriter"
+	"time"
 
 	v1 "k8s.io/api/core/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+
+	"github.com/kinvolk/lokomotive/pkg/util/retryutil"
+)
+
+const (
+	// Max number of retries when waiting for cluster to become available.
+	clusterPingRetries = 18
+	// Number of seconds to wait between retires when waiting for cluster to become available.
+	clusterPingRetryInterval = 10
+	// Max number of retries when waiting for nodes to become ready.
+	nodeReadinessRetries = 18
+	// Number of seconds to wait between retires when waiting for nodes to become ready.
+	nodeReadinessRetryInterval = 10
 )
 
 type Cluster struct {
@@ -131,4 +145,38 @@ func (cl *Cluster) Ping() (bool, error) {
 		return false, nil
 	}
 	return true, nil
+}
+
+func (cl *Cluster) Verify() error {
+	fmt.Println("\nNow checking health and readiness of the cluster nodes ...")
+
+	// Wait for cluster to become available
+	err := retryutil.Retry(clusterPingRetryInterval*time.Second, clusterPingRetries, cl.Ping)
+	if err != nil {
+		return fmt.Errorf("pinging cluster for readiness: %w", err)
+	}
+
+	var ns *NodeStatus
+	var nsErr error
+	err = retryutil.Retry(nodeReadinessRetryInterval*time.Second, nodeReadinessRetries, func() (bool, error) {
+		// Store the original error because Retry would stop too early if we forward it
+		// and anyway overrides the error in case of timeout.
+		ns, nsErr = cl.GetNodeStatus()
+		if nsErr != nil {
+			// To continue retrying, we don't set the error here.
+			return false, nil
+		}
+		return ns.Ready(), nil // Retry if not ready
+	})
+	if nsErr != nil {
+		return fmt.Errorf("waiting for nodes: %w", nsErr)
+	}
+	if err != nil {
+		return fmt.Errorf("not all nodes became ready within the allowed time")
+	}
+	ns.PrettyPrint()
+
+	fmt.Println("\nSuccess - cluster is healthy and nodes are ready!")
+
+	return nil
 }
