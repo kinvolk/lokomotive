39e3a898fa955e8493d8ffca187ca5736d6d1050
Johannes Liebermann <johannes@kinvolk.io>
2020-09-15 12:49:07 +0200

    components/rook: Refactor conversion test

--- pkg/components/rook/component_conversion_test.go
+++ pkg/components/rook/component_conversion_test.go
@@ -15,12 +15,12 @@
 package rook_test
 
 import (
-	"reflect"
+	"fmt"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/hcl/v2"
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/yaml"
 
 	"github.com/kinvolk/lokomotive/pkg/components"
@@ -29,72 +29,281 @@ import (
 
 //nolint:funlen
 func TestConversion(t *testing.T) {
-	configHCL := `
-component "rook" {
-  enable_monitoring = true
-
-  node_selector = {
-    "storage_node_key1" = "storage_node_value1"
-    "storage_node_key2" = "storage_node_value2"
-  }
-
-  toleration {
-    key      = "toleration_key1"
-    operator = "Equal"
-    value    = "toleration_value1"
-    effect   = "NoSchedule"
-  }
-
-  toleration {
-    key      = "toleration_key2"
-    operator = "Equal"
-    value    = "toleration_value2"
-    effect   = "NoSchedule"
-  }
-
-  agent_toleration_key    = "agent_toleration_key"
-  agent_toleration_effect = "NoSchedule"
-
-  discover_toleration_key    = "discover_toleration_key"
-  discover_toleration_effect = "NoSchedule"
-
-  csi_plugin_node_selector = {
-    "node_key1" = "node_value1"
-    "node_key2" = "node_value2"
-  }
-
-  csi_plugin_toleration {
-    key = "other_node_key1"
-    operator = "Equal"
-    value = "other_node_value1"
-    effect = "NoSchedule"
-  }
-}
-`
-
-	m := renderManifests(t, configHCL)
-	if len(m) == 0 {
-		t.Fatalf("Rendered manifests shouldn't be empty")
-	}
-
-	deploy := getOperatorDeployment(t, m)
-
 	tcs := []struct {
-		Name string
-		Test func(*testing.T, map[string]string, *appsv1.Deployment)
+		desc           string
+		config         string
+		wantDeployment string
 	}{
-		{"MemoryResources", memoryResources},
-		{"NodeSelector", nodeSelector},
-		{"Monitoring", monitoring},
-		{"OperatorTolerations", operatorTolerations},
-		{"EnvVars", envVars},
+		{
+			desc:   "Basic",
+			config: `component "rook" {}`,
+			wantDeployment: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: rook-ceph-operator
+  labels:
+    operator: rook
+    storage-backend: ceph
+    chart: "rook-ceph-v1.4.2"
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: rook-ceph-operator
+  template:
+    metadata:
+      labels:
+        app: rook-ceph-operator
+        chart: "rook-ceph-v1.4.2"
+    spec:
+      containers:
+      - name: rook-ceph-operator
+        image: "rook/ceph:v1.4.2"
+        imagePullPolicy: IfNotPresent
+        args: ["ceph", "operator"]
+        env:
+        - name: ROOK_CURRENT_NAMESPACE_ONLY
+          value: "false"
+        - name: FLEXVOLUME_DIR_PATH
+          value: /var/lib/kubelet/volumeplugins
+        - name: ROOK_HOSTPATH_REQUIRES_PRIVILEGED
+          value: "false"
+        - name: ROOK_LOG_LEVEL
+          value: INFO
+        - name: ROOK_ENABLE_SELINUX_RELABELING
+          value: "true"
+        - name: ROOK_DISABLE_DEVICE_HOTPLUG
+          value: "false"
+        - name: ROOK_CSI_ENABLE_RBD
+          value: "true"
+        - name: ROOK_CSI_ENABLE_CEPHFS
+          value: "true"
+        - name: CSI_PLUGIN_PRIORITY_CLASSNAME
+          value: 
+        - name: CSI_PROVISIONER_PRIORITY_CLASSNAME
+          value: 
+        - name: ROOK_CSI_ENABLE_GRPC_METRICS
+          value: "true"
+        - name: CSI_CEPHFS_GRPC_METRICS_PORT
+          value: "9092"
+        - name: CSI_FORCE_CEPHFS_KERNEL_CLIENT
+          value: "true"
+        - name: ROOK_ENABLE_FLEX_DRIVER
+          value: "false"
+        - name: ROOK_ENABLE_DISCOVERY_DAEMON
+          value: "true"
+        - name: ROOK_OBC_WATCH_OPERATOR_NAMESPACE
+          value: "true"
+        - name: NODE_NAME
+          valueFrom:
+            fieldRef:
+              fieldPath: spec.nodeName
+        - name: POD_NAME
+          valueFrom:
+            fieldRef:
+              fieldPath: metadata.name
+        - name: POD_NAMESPACE
+          valueFrom:
+            fieldRef:
+              fieldPath: metadata.namespace
+        - name: ROOK_UNREACHABLE_NODE_TOLERATION_SECONDS
+          value: "5"
+        resources:
+          limits:
+            cpu: 500m
+            memory: 512Mi
+          requests:
+            cpu: 100m
+            memory: 256Mi
+      serviceAccountName: rook-ceph-system`,
+		},
+
+		{
+			desc: "All knobs",
+			config: `component "rook" {
+		  enable_monitoring = true
+
+		  node_selector = {
+		    "storage_node_key1" = "storage_node_value1"
+		    "storage_node_key2" = "storage_node_value2"
+		  }
+
+		  toleration {
+		    key      = "toleration_key1"
+		    operator = "Equal"
+		    value    = "toleration_value1"
+		    effect   = "NoSchedule"
+		  }
+
+		  toleration {
+		    key      = "toleration_key2"
+		    operator = "Equal"
+		    value    = "toleration_value2"
+		    effect   = "NoSchedule"
+		  }
+
+		  agent_toleration_key    = "agent_toleration_key"
+		  agent_toleration_effect = "NoSchedule"
+
+		  discover_toleration_key    = "discover_toleration_key"
+		  discover_toleration_effect = "NoSchedule"
+
+		  csi_plugin_node_selector = {
+		    "node_key1" = "node_value1"
+		    "node_key2" = "node_value2"
+		  }
+
+		  csi_plugin_toleration {
+		    key = "other_node_key1"
+		    operator = "Equal"
+		    value = "other_node_value1"
+		    effect = "NoSchedule"
+		  }
+		}`,
+			wantDeployment: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: rook-ceph-operator
+  labels:
+    operator: rook
+    storage-backend: ceph
+    chart: "rook-ceph-v1.4.2"
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: rook-ceph-operator
+  template:
+    metadata:
+      labels:
+        app: rook-ceph-operator
+        chart: "rook-ceph-v1.4.2"
+    spec:
+      containers:
+      - name: rook-ceph-operator
+        image: "rook/ceph:v1.4.2"
+        imagePullPolicy: IfNotPresent
+        args: ["ceph", "operator"]
+        env:
+        - name: ROOK_CURRENT_NAMESPACE_ONLY
+          value: "false"
+        - name: AGENT_TOLERATION
+          value: NoSchedule
+        - name: AGENT_TOLERATION_KEY
+          value: agent_toleration_key
+        - name: AGENT_TOLERATIONS
+          value: "- effect: NoSchedule\n  key: toleration_key1\n  operator: Equal\n  value: toleration_value1\n- effect: NoSchedule\n  key: toleration_key2\n  operator: Equal\n  value: toleration_value2"
+        - name: AGENT_NODE_AFFINITY
+          value: storage_node_key1=storage_node_value1; storage_node_key2=storage_node_value2;
+        - name: FLEXVOLUME_DIR_PATH
+          value: /var/lib/kubelet/volumeplugins
+        - name: DISCOVER_TOLERATION
+          value: NoSchedule
+        - name: DISCOVER_TOLERATION_KEY
+          value: discover_toleration_key
+        - name: DISCOVER_TOLERATIONS
+          value: "- effect: NoSchedule\n  key: toleration_key1\n  operator: Equal\n  value: toleration_value1\n- effect: NoSchedule\n  key: toleration_key2\n  operator: Equal\n  value: toleration_value2"
+        - name: DISCOVER_AGENT_NODE_AFFINITY
+          value: storage_node_key1=storage_node_value1; storage_node_key2=storage_node_value2;
+        - name: ROOK_HOSTPATH_REQUIRES_PRIVILEGED
+          value: "false"
+        - name: ROOK_LOG_LEVEL
+          value: INFO
+        - name: ROOK_ENABLE_SELINUX_RELABELING
+          value: "true"
+        - name: ROOK_DISABLE_DEVICE_HOTPLUG
+          value: "false"
+        - name: ROOK_CSI_ENABLE_RBD
+          value: "true"
+        - name: ROOK_CSI_ENABLE_CEPHFS
+          value: "true"
+        - name: CSI_PLUGIN_PRIORITY_CLASSNAME
+          value:
+        - name: CSI_PROVISIONER_PRIORITY_CLASSNAME
+          value:
+        - name: ROOK_CSI_ENABLE_GRPC_METRICS
+          value: "true"
+        - name: CSI_PROVISIONER_TOLERATIONS
+          value: "- effect: NoSchedule\n  key: toleration_key1\n  operator: Equal\n  value: toleration_value1\n- effect: NoSchedule\n  key: toleration_key2\n  operator: Equal\n  value: toleration_value2"
+        - name: CSI_PROVISIONER_NODE_AFFINITY
+          value: storage_node_key1=storage_node_value1; storage_node_key2=storage_node_value2;
+        - name: CSI_PLUGIN_TOLERATIONS
+          value: "- effect: NoSchedule\n  key: other_node_key1\n  operator: Equal\n  value: other_node_value1"
+        - name: CSI_PLUGIN_NODE_AFFINITY
+          value: node_key1=node_value1; node_key2=node_value2;
+        - name: CSI_CEPHFS_GRPC_METRICS_PORT
+          value: "9092"
+        - name: CSI_FORCE_CEPHFS_KERNEL_CLIENT
+          value: "true"
+        - name: ROOK_ENABLE_FLEX_DRIVER
+          value: "false"
+        - name: ROOK_ENABLE_DISCOVERY_DAEMON
+          value: "true"
+        - name: ROOK_OBC_WATCH_OPERATOR_NAMESPACE
+          value: "true"
+        - name: NODE_NAME
+          valueFrom:
+            fieldRef:
+              fieldPath: spec.nodeName
+        - name: POD_NAME
+          valueFrom:
+            fieldRef:
+              fieldPath: metadata.name
+        - name: POD_NAMESPACE
+          valueFrom:
+            fieldRef:
+              fieldPath: metadata.namespace
+        - name: ROOK_UNREACHABLE_NODE_TOLERATION_SECONDS
+          value: "5"
+        resources:
+          limits:
+            cpu: 500m
+            memory: 512Mi
+          requests:
+            cpu: 100m
+            memory: 256Mi
+      nodeSelector:
+        storage_node_key1: storage_node_value1
+        storage_node_key2: storage_node_value2
+      tolerations:
+        - effect: NoSchedule
+          key: toleration_key1
+          operator: Equal
+          value: toleration_value1
+        - effect: NoSchedule
+          key: toleration_key2
+          operator: Equal
+          value: toleration_value2
+      serviceAccountName: rook-ceph-system`,
+		},
 	}
 
 	for _, tc := range tcs {
 		tc := tc
-		t.Run(tc.Name, func(t *testing.T) {
-			t.Parallel()
-			tc.Test(t, m, deploy)
+
+		t.Run(tc.desc, func(t *testing.T) {
+			// Using t.Parallel() causes data races on the component's data structure because
+			// multiple tests are unmarshaling configuration into the same struct in memory.
+			// t.Parallel()
+
+			m := renderManifests(t, tc.config)
+			if len(m) == 0 {
+				t.Fatalf("Rendered manifests shouldn't be empty")
+			}
+
+			got, err := deploymentFromYAML(m["rook-ceph/templates/deployment.yaml"])
+			if err != nil {
+				t.Fatalf("Unmarshaling deployment: %v", err)
+			}
+
+			want, err := deploymentFromYAML(tc.wantDeployment)
+			if err != nil {
+				t.Fatalf("Unmarshaling deployment: %v", err)
+			}
+
+			if diff := cmp.Diff(got, want); diff != "" {
+				t.Fatalf("%q: unexpected deployment (-want +got)\n%s", tc.desc, diff)
+			}
 		})
 	}
 }
@@ -125,107 +334,16 @@ func renderManifests(t *testing.T, configHCL string) map[string]string {
 	return ret
 }
 
-func getOperatorDeployment(t *testing.T, m map[string]string) *appsv1.Deployment {
-	deployStr, ok := m["rook-ceph/templates/deployment.yaml"]
-	if !ok {
-		t.Fatalf("Operator deployment config not found")
-	}
+func int32Ptr(i int32) *int32 {
+	return &i
+}
 
+func deploymentFromYAML(s string) (*appsv1.Deployment, error) {
 	deploy := &appsv1.Deployment{}
-	if err := yaml.Unmarshal([]byte(deployStr), deploy); err != nil {
-		t.Fatalf("Unmarshaling manifest: %v", err)
+
+	if err := yaml.Unmarshal([]byte(s), deploy); err != nil {
+		return nil, fmt.Errorf("Unmarshaling manifest: %v", err)
 	}
 
-	return deploy
-}
-
-func memoryResources(t *testing.T, m map[string]string, deploy *appsv1.Deployment) {
-	expected := "512Mi"
-
-	got := deploy.Spec.Template.Spec.Containers[0].Resources.Limits.Memory().String()
-	if expected != got {
-		t.Fatalf("Expected: %s, got: %s", expected, got)
-	}
-}
-
-func nodeSelector(t *testing.T, m map[string]string, deploy *appsv1.Deployment) {
-	expected := map[string]string{
-		"storage_node_key1": "storage_node_value1",
-		"storage_node_key2": "storage_node_value2",
-	}
-	got := deploy.Spec.Template.Spec.NodeSelector
-
-	if !reflect.DeepEqual(expected, got) {
-		t.Fatalf("Expected: %v, got: %v", expected, got)
-	}
-}
-
-func monitoring(t *testing.T, m map[string]string, deploy *appsv1.Deployment) {
-	expectedConfig := []string{
-		"rook-ceph/templates/ceph-cluster.yaml",
-		"rook-ceph/templates/ceph-osd.yaml",
-		"rook-ceph/templates/ceph-pools.yaml",
-		"rook-ceph/templates/csi-metrics-service-monitor.yaml",
-		"rook-ceph/templates/prometheus-ceph-v14-rules.yaml",
-		"rook-ceph/templates/service-monitor.yaml",
-	}
-
-	for _, ec := range expectedConfig {
-		if _, ok := m[ec]; !ok {
-			t.Fatalf("Expected %q to be generated but it is not available", ec)
-		}
-	}
-}
-
-func operatorTolerations(t *testing.T, m map[string]string, deploy *appsv1.Deployment) {
-	expected := []corev1.Toleration{
-		{Key: "toleration_key1", Operator: "Equal", Value: "toleration_value1", Effect: "NoSchedule"},
-		{Key: "toleration_key2", Operator: "Equal", Value: "toleration_value2", Effect: "NoSchedule"},
-	}
-	got := deploy.Spec.Template.Spec.Tolerations
-
-	if !reflect.DeepEqual(expected, got) {
-		t.Fatalf("Expected: %v, got: %v", expected, got)
-	}
-}
-
-func envVars(t *testing.T, m map[string]string, deploy *appsv1.Deployment) {
-	//nolint:lll
-	tcs := map[string]string{
-		"AGENT_TOLERATION_KEY":          "agent_toleration_key",
-		"AGENT_TOLERATION":              "NoSchedule",
-		"AGENT_TOLERATIONS":             "- effect: NoSchedule\n  key: toleration_key1\n  operator: Equal\n  value: toleration_value1\n- effect: NoSchedule\n  key: toleration_key2\n  operator: Equal\n  value: toleration_value2",
-		"DISCOVER_TOLERATION_KEY":       "discover_toleration_key",
-		"DISCOVER_TOLERATION":           "NoSchedule",
-		"DISCOVER_TOLERATIONS":          "- effect: NoSchedule\n  key: toleration_key1\n  operator: Equal\n  value: toleration_value1\n- effect: NoSchedule\n  key: toleration_key2\n  operator: Equal\n  value: toleration_value2",
-		"CSI_PROVISIONER_TOLERATIONS":   "- effect: NoSchedule\n  key: toleration_key1\n  operator: Equal\n  value: toleration_value1\n- effect: NoSchedule\n  key: toleration_key2\n  operator: Equal\n  value: toleration_value2",
-		"CSI_PLUGIN_TOLERATIONS":        "- effect: NoSchedule\n  key: other_node_key1\n  operator: Equal\n  value: other_node_value1",
-		"AGENT_NODE_AFFINITY":           "storage_node_key1=storage_node_value1; storage_node_key2=storage_node_value2;",
-		"DISCOVER_AGENT_NODE_AFFINITY":  "storage_node_key1=storage_node_value1; storage_node_key2=storage_node_value2;",
-		"CSI_PROVISIONER_NODE_AFFINITY": "storage_node_key1=storage_node_value1; storage_node_key2=storage_node_value2;",
-		"CSI_PLUGIN_NODE_AFFINITY":      "node_key1=node_value1; node_key2=node_value2;",
-	}
-
-	envs := deploy.Spec.Template.Spec.Containers[0].Env
-
-	// Convert the envs into a map so that comparison becomes easier.
-	envVars := map[string]string{}
-	for _, en := range envs {
-		envVars[en.Name] = en.Value
-	}
-
-	// For every expected key-value pair, ensure it exists in the converted output.
-	for k, v := range tcs {
-		k, v := k, v
-		t.Run(k, func(t *testing.T) {
-			gotVal, ok := envVars[k]
-			if !ok {
-				t.Fatalf("Expected: %q env", k)
-			}
-
-			if gotVal != v {
-				t.Fatalf("Expected env var %q:%q, got %q:%q", k, v, k, gotVal)
-			}
-		})
-	}
+	return deploy, nil
 }
