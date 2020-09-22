de70624d0cf8edbd96b17922f5212281fdc3508b
Suraj Deshmukh <suraj@kinvolk.io>
2020-09-22 15:11:05 +0530

    prometheus-operator: Add conversion tests
    - This commit adds new conversion test to verify if the external_url
      works fine.
    
    - This commit adds test cases in TestRenderManifest to verify if the
      prometheus external_url and prometheus ingress host matches.
    
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

---
+++ pkg/components/internal/testutil/common.go
@@ -0,0 +1,61 @@
+// Copyright 2020 The Lokomotive Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testutil
+
+import (
+	"testing"
+
+	"github.com/hashicorp/hcl/v2"
+
+	"github.com/kinvolk/lokomotive/pkg/components"
+	"github.com/kinvolk/lokomotive/pkg/components/util"
+)
+
+// ConfigFromMap takes a map and a key and returns the value associated with the key. If the key
+// does not exist in that map, the function fails.
+func ConfigFromMap(t *testing.T, m map[string]string, k string) string {
+	ret, ok := m[k]
+	if !ok {
+		t.Fatalf("Config not found with filename: %q", k)
+	}
+
+	return ret
+}
+
+// RenderManifests converts a component into YAML manifests.
+func RenderManifests(
+	t *testing.T, component components.Component, componentName string, hclConfig string,
+) map[string]string {
+	body, diagnostics := util.GetComponentBody(hclConfig, componentName)
+	if diagnostics.HasErrors() {
+		t.Fatalf("Getting component body: %v", diagnostics.Errs())
+	}
+
+	diagnostics = component.LoadConfig(body, &hcl.EvalContext{})
+	if diagnostics.HasErrors() {
+		t.Fatalf("Loading configuration: %v", diagnostics)
+	}
+
+	ret, err := component.RenderManifests()
+	if err != nil {
+		t.Fatalf("Rendering manifests: %v", err)
+	}
+
+	if len(ret) == 0 {
+		t.Fatalf("Rendered manifests shouldn't be empty")
+	}
+
+	return ret
+}
