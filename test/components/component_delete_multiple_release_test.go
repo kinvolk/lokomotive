d808ec0f651dfa180339a6c6264bbc7d3d5f8d7a
knrt10 <kautilya@kinvolk.io>
2020-10-19 16:00:56 +0530

    Add disruptivee2e for component delete
    Headlamp is installed in `lokomotive-system` so deleting it along with
    namespace should throw error.
    
    Signed-off-by: knrt10 <kautilya@kinvolk.io>

---
+++ test/components/component_delete_multiple_release_test.go
@@ -0,0 +1,42 @@
+// Copyright 2020 The Lokomotive Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build aws aws_edge packet
+// +build disruptivee2e
+
+package components_test
+
+import (
+	"testing"
+
+	"github.com/kinvolk/lokomotive/pkg/components"
+	_ "github.com/kinvolk/lokomotive/pkg/components/headlamp"
+	"github.com/kinvolk/lokomotive/pkg/components/util"
+	testutil "github.com/kinvolk/lokomotive/test/components/util"
+)
+
+func TestDeleteNamespaceMultipleRelease(t *testing.T) {
+	n := "headlamp"
+
+	c, err := components.Get(n)
+	if err != nil {
+		t.Fatalf("failed getting component: %v", err)
+	}
+
+	k := testutil.Kubeconfig(t)
+
+	if err := util.UninstallComponent(c, k, true); err == nil {
+		t.Fatalf("Deleting component should fail.")
+	}
+}
