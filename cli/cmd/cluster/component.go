0d0db10aae111db971efb1650ad48890775df19d
Mateusz Gozdek <mateusz@kinvolk.io>
2020-11-02 21:09:04 +0100

    cli/cmd/cluster: drop dependency on pkg/components.ListNames()
    Instead, add few helper functions, which will also replace
    components.Get().
    
    Refs #992
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- cli/cmd/cluster/component.go
+++ cli/cmd/cluster/component.go
@@ -15,33 +15,74 @@
 package cluster
 
 import (
-	// Register a component by adding an anonymous import.
-	_ "github.com/kinvolk/lokomotive/pkg/components/aws-ebs-csi-driver"
-	_ "github.com/kinvolk/lokomotive/pkg/components/cert-manager"
-	_ "github.com/kinvolk/lokomotive/pkg/components/cluster-autoscaler"
-	_ "github.com/kinvolk/lokomotive/pkg/components/contour"
-	_ "github.com/kinvolk/lokomotive/pkg/components/dex"
-	_ "github.com/kinvolk/lokomotive/pkg/components/external-dns"
-	_ "github.com/kinvolk/lokomotive/pkg/components/flatcar-linux-update-operator"
-	_ "github.com/kinvolk/lokomotive/pkg/components/gangway"
-	_ "github.com/kinvolk/lokomotive/pkg/components/httpbin"
-	_ "github.com/kinvolk/lokomotive/pkg/components/inspektor-gadget"
-	_ "github.com/kinvolk/lokomotive/pkg/components/istio-operator"
-	_ "github.com/kinvolk/lokomotive/pkg/components/linkerd"
-	_ "github.com/kinvolk/lokomotive/pkg/components/metallb"
-	_ "github.com/kinvolk/lokomotive/pkg/components/metrics-server"
-	_ "github.com/kinvolk/lokomotive/pkg/components/openebs-operator"
-	_ "github.com/kinvolk/lokomotive/pkg/components/openebs-storage-class"
-	_ "github.com/kinvolk/lokomotive/pkg/components/prometheus-operator"
-	_ "github.com/kinvolk/lokomotive/pkg/components/rook"
-	_ "github.com/kinvolk/lokomotive/pkg/components/rook-ceph"
-	_ "github.com/kinvolk/lokomotive/pkg/components/velero"
-	_ "github.com/kinvolk/lokomotive/pkg/components/web-ui"
+	"fmt"
 
 	"github.com/kinvolk/lokomotive/pkg/components"
+	awsebscsidriver "github.com/kinvolk/lokomotive/pkg/components/aws-ebs-csi-driver"
+	certmanager "github.com/kinvolk/lokomotive/pkg/components/cert-manager"
+	clusterautoscaler "github.com/kinvolk/lokomotive/pkg/components/cluster-autoscaler"
+	"github.com/kinvolk/lokomotive/pkg/components/contour"
+	"github.com/kinvolk/lokomotive/pkg/components/dex"
+	externaldns "github.com/kinvolk/lokomotive/pkg/components/external-dns"
+	flatcarlinuxupdateoperator "github.com/kinvolk/lokomotive/pkg/components/flatcar-linux-update-operator"
+	"github.com/kinvolk/lokomotive/pkg/components/gangway"
+	"github.com/kinvolk/lokomotive/pkg/components/httpbin"
+	inspektorgadget "github.com/kinvolk/lokomotive/pkg/components/inspektor-gadget"
+	istiooperator "github.com/kinvolk/lokomotive/pkg/components/istio-operator"
+	"github.com/kinvolk/lokomotive/pkg/components/linkerd"
+	"github.com/kinvolk/lokomotive/pkg/components/metallb"
+	metricsserver "github.com/kinvolk/lokomotive/pkg/components/metrics-server"
+	openebsoperator "github.com/kinvolk/lokomotive/pkg/components/openebs-operator"
+	openebsstorageclass "github.com/kinvolk/lokomotive/pkg/components/openebs-storage-class"
+	"github.com/kinvolk/lokomotive/pkg/components/prometheus-operator"
+	"github.com/kinvolk/lokomotive/pkg/components/rook"
+	rookceph "github.com/kinvolk/lokomotive/pkg/components/rook-ceph"
+	"github.com/kinvolk/lokomotive/pkg/components/velero"
+	webui "github.com/kinvolk/lokomotive/pkg/components/web-ui"
 )
+
+func componentsConfigs() map[string]components.Component {
+	return map[string]components.Component{
+		awsebscsidriver.Name:            awsebscsidriver.NewConfig(),
+		certmanager.Name:                certmanager.NewConfig(),
+		clusterautoscaler.Name:          clusterautoscaler.NewConfig(),
+		contour.Name:                    contour.NewConfig(),
+		dex.Name:                        dex.NewConfig(),
+		externaldns.Name:                externaldns.NewConfig(),
+		flatcarlinuxupdateoperator.Name: flatcarlinuxupdateoperator.NewConfig(),
+		gangway.Name:                    gangway.NewConfig(),
+		httpbin.Name:                    httpbin.NewConfig(),
+		inspektorgadget.Name:            inspektorgadget.NewConfig(),
+		istiooperator.Name:              istiooperator.NewConfig(),
+		linkerd.Name:                    linkerd.NewConfig(),
+		metallb.Name:                    metallb.NewConfig(),
+		metricsserver.Name:              metricsserver.NewConfig(),
+		openebsoperator.Name:            openebsoperator.NewConfig(),
+		openebsstorageclass.Name:        openebsstorageclass.NewConfig(),
+		prometheus.Name:                 prometheus.NewConfig(),
+		rook.Name:                       rook.NewConfig(),
+		rookceph.Name:                   rookceph.NewConfig(),
+		velero.Name:                     velero.NewConfig(),
+		webui.Name:                      webui.NewConfig(),
+	}
+}
 
 // AvailableComponents returns list of valid component names.
 func AvailableComponents() []string {
-	return components.ListNames()
+	c := []string{}
+
+	for n := range componentsConfigs() {
+		c = append(c, n)
+	}
+
+	return c
+}
+
+func componentConfig(name string) (components.Component, error) {
+	c, ok := componentsConfigs()[name]
+	if !ok {
+		return nil, fmt.Errorf("no component with name %q found", name)
+	}
+
+	return c, nil
 }
