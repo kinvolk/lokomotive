c1eaaff500c2ea63a322614c25360f8a349f42ab
Mateusz Gozdek <mateusz@kinvolk.io>
2020-11-13 17:10:43 +0100

    pkg/platform: improve tests for CommonControlPlaneCharts
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- pkg/platform/platform_test.go
+++ pkg/platform/platform_test.go
@@ -15,7 +15,6 @@
 package platform_test
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/kinvolk/lokomotive/pkg/platform"
@@ -47,26 +46,111 @@ func TestAppendVersionTag(t *testing.T) {
 	}
 }
 
-func TestCommonControlPlaneChartsOrder(t *testing.T) {
-	expectedOrder := []string{
-		"bootstrap-secrets",
-		"pod-checkpointer",
-		"kube-apiserver",
-		"kubernetes",
-		"calico",
-		"lokomotive",
-		"kubelet",
+func TestAtLeastOneCommonControlplaneChartIsDefined(t *testing.T) {
+	if len(platform.CommonControlPlaneCharts(false)) == 0 {
+		t.Fatalf("There should be at least one common controlplane chart defined")
+	}
+}
+
+func TestBootstrapSecretsAreUpdatedFirst(t *testing.T) {
+	if platform.CommonControlPlaneCharts(true)[0].Name != "bootstrap-secrets" {
+		t.Fatalf("Bootstrap-secrets should be updated first to allow nodes to proceed with bootstrapping process")
+	}
+}
+
+func TestPodCheckpointerIsUpdatedBeforeKubeApiserver(t *testing.T) {
+	podCheckpointerFound := false
+
+	for _, c := range platform.CommonControlPlaneCharts(true) {
+		if c.Name == "pod-checkpointer" {
+			podCheckpointerFound = true
+		}
+
+		if c.Name == "kube-apiserver" && !podCheckpointerFound {
+			t.Fatalf("Pod-checkpointer should be updated before kube-apiserver to ensure API availability if " +
+				"upgrade process of kube-apiserver fails for some reason.")
+		}
+	}
+}
+
+func TestKubeApiserverIsUpdatedBeforeOtherKubernetesComponents(t *testing.T) {
+	kubeAPIServerFound := false
+
+	for _, c := range platform.CommonControlPlaneCharts(true) {
+		if c.Name == "kube-apiserver" {
+			kubeAPIServerFound = true
+		}
+
+		if c.Name == "kubernetes" && !kubeAPIServerFound {
+			t.Fatalf("Kube-apiserver must be updated before other Kubernetes components to ensure " +
+				"Kubernetes version skew support policy")
+		}
+	}
+}
+
+func TestCalicoIsUpdatedAfterKubernetesComponents(t *testing.T) {
+	kubernetesFound := false
+
+	for _, c := range platform.CommonControlPlaneCharts(true) {
+		if c.Name == "kubernetes" {
+			kubernetesFound = true
+		}
+
+		if c.Name == "calico" && !kubernetesFound {
+			t.Fatalf("Calico must be updated after Kubernetes component, as it requires functional kube-proxy " +
+				"to converge")
+		}
+	}
+}
+
+func TestKubeletIsUpdatedAfterOtherKubernetesComponents(t *testing.T) {
+	kubernetesFound := false
+
+	for _, c := range platform.CommonControlPlaneCharts(true) {
+		if c.Name == "kubernetes" {
+			kubernetesFound = true
+		}
+
+		if c.Name == "kubelet" && !kubernetesFound {
+			t.Fatalf("kubelet must be updated after Kubernetes component to ensure Kubernetes version skew support policy")
+		}
+	}
+}
+
+func TestLokomotiveIsUpdatedAfterCalico(t *testing.T) {
+	calicoFound := false
+
+	for _, c := range platform.CommonControlPlaneCharts(true) {
+		if c.Name == "calico" {
+			calicoFound = true
+		}
+
+		if c.Name == "lokomotive" && !calicoFound {
+			t.Fatalf("Lokomotive must be updated after Calico, as it requires Pod networking to converge")
+		}
+	}
+}
+
+func TestKubeletIsExcludedFromUpdatesWhenNotRequested(t *testing.T) {
+	for _, c := range platform.CommonControlPlaneCharts(false) {
+		if c.Name == "kubelet" {
+			t.Fatalf("Kubelet should not be included in charts list when not requested")
+		}
+	}
+}
+
+func TestKubeletIsIncludedInCommonChartsWhenRequested(t *testing.T) {
+	kubeletFound := false
+
+	for _, c := range platform.CommonControlPlaneCharts(true) {
+		if c.Name == "kubelet" {
+			kubeletFound = true
+
+			break
+		}
 	}
 
-	commonControlPlaneCharts := platform.CommonControlPlaneCharts(true)
-
-	actualOrder := []string{}
-
-	for _, v := range commonControlPlaneCharts {
-		actualOrder = append(actualOrder, v.Name)
-	}
-
-	if !reflect.DeepEqual(actualOrder, expectedOrder) {
-		t.Fatalf("expected order: %s, got: %s", expectedOrder, actualOrder)
+	if !kubeletFound {
+		t.Fatalf("Kubelet should be included in charts list when requested")
 	}
 }
