269723f49634b3055c0655c035e9dfc8dc39405e
Mateusz Gozdek <mateusz@kinvolk.io>
2020-11-30 12:07:35 +0100

    pod-checkpointer: update image to latest version and pull from Quay
    To avoid hitting Docker Hub pull limits.
    
    Part of #1225.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- assets/terraform-modules/bootkube/variables.tf
+++ assets/terraform-modules/bootkube/variables.tf
@@ -95,7 +95,7 @@ variable "container_images" {
     flexvol_driver_image    = "quay.io/kinvolk/calico-pod2daemon-flexvol:v3.16.4"
     kubelet_image           = "quay.io/poseidon/kubelet:v1.19.4"
     coredns                 = "quay.io/kinvolk/coredns:1.8.0"
-    pod_checkpointer        = "kinvolk/pod-checkpointer:d1c58443fe7d7d33aa5bf7d80d65d299be6e5847"
+    pod_checkpointer        = "quay.io/kinvolk/checkpoint:43ec4b414e44f202e07bf43e57d2b5ffbcfd4415"
     kube_apiserver          = "k8s.gcr.io/kube-apiserver:v1.19.4"
     kube_controller_manager = "k8s.gcr.io/kube-controller-manager:v1.19.4"
     kube_scheduler          = "k8s.gcr.io/kube-scheduler:v1.19.4"
