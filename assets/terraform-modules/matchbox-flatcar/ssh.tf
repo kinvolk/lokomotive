edc266b6655cd5aa98a988e9c3fa16c735bbf938
Kai Lüke <kai@kinvolk.io>
2021-02-15 17:14:11 +0100

    baremetal: run copy-controller-secrets step after recreating the node

--- assets/terraform-modules/matchbox-flatcar/ssh.tf
+++ assets/terraform-modules/matchbox-flatcar/ssh.tf
@@ -7,6 +7,6 @@ resource "null_resource" "reprovision-node-when-ignition-changes" {
   depends_on = [matchbox_group.node]
   # Trigger running Ignition on the next reboot (first_boot flag file) and reboot the instance, or, if the instance needs to be (re)provisioned, run external commands for PXE booting (also runs on the first provisioning)
   provisioner "local-exec" {
-    command = templatefile("${path.module}/pxe-helper.sh.tmpl", { domain = var.node_domain, name = var.node_name, mac = var.node_mac, pxe_commands = var.pxe_commands, asset_dir = var.asset_dir, type = "node" })
+    command = templatefile("${path.module}/pxe-helper.sh.tmpl", { domain = var.node_domain, name = var.node_name, mac = var.node_mac, pxe_commands = var.pxe_commands, asset_dir = var.asset_dir, kernel_args = join(" ", var.kernel_args), kernel_console = join(" ", var.kernel_console), ignition_endpoint = format("%s/ignition", var.http_endpoint), type = "node" })
   }
 }
