c90050a6de288f827456cb1cf20cfde150dbcb88
Kai Lüke <kai@kinvolk.io>
2021-02-16 18:31:59 +0100

    baremetal: update the kernel arguments when skipping a PXE boot
    When the PXE boot is skipped, the new Ignition config is already
    fetched but it does not contain the kernel arguments because these
    have to be set outside of Ignition or otherwise they would only
    apply to the following boots. The kernel arguments are kept in the
    grub config and this needs to be updated out of band (kernel console
    and Ignition URL are also part of the kernel arguments).

--- assets/terraform-modules/matchbox-flatcar/ssh.tf
+++ assets/terraform-modules/matchbox-flatcar/ssh.tf
@@ -2,11 +2,13 @@ resource "null_resource" "reprovision-node-when-ignition-changes" {
   # Triggered when the Ignition Config changes
   triggers = {
     ignition_config = matchbox_profile.node.raw_ignition
+    kernel_args = join(" ", var.kernel_args)
+    kernel_console = join(" ", var.kernel_console)
   }
   # Wait for the new Ignition config object to be ready before rebooting
   depends_on = [matchbox_group.node]
   # Trigger running Ignition on the next reboot (first_boot flag file) and reboot the instance, or, if the instance needs to be (re)provisioned, run external commands for PXE booting (also runs on the first provisioning)
   provisioner "local-exec" {
-    command = templatefile("${path.module}/pxe-helper.sh.tmpl", { domain = var.node_domain, name = var.node_name, mac = var.node_mac, pxe_commands = var.pxe_commands, asset_dir = var.asset_dir, ignore_changes = var.ignore_changes })
+    command = templatefile("${path.module}/pxe-helper.sh.tmpl", { domain = var.node_domain, name = var.node_name, mac = var.node_mac, pxe_commands = var.pxe_commands, asset_dir = var.asset_dir, kernel_args = join(" ", var.kernel_args), kernel_console = join(" ", var.kernel_console), ignition_endpoint = format("%s/ignition", var.http_endpoint), ignore_changes = var.ignore_changes })
   }
 }
