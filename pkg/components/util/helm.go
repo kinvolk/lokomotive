da44d652546a7ad30bff09665e904df2be61f838
Jean-Baptiste Alleaume <jbanety@gmail.com>
2021-03-01 08:04:53 +0100

    Simpify
    Co-authored-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- pkg/components/util/helm.go
+++ pkg/components/util/helm.go
@@ -59,11 +59,11 @@ func RenderChart(helmChart *chart.Chart, name, namespace, values string) (map[st
 
 	// Include hooks
 	for _, m := range template.Hooks {
-		if val, ok := ret[m.Path]; ok {
-			ret[m.Path] = val + "\n---\n" + m.Manifest
-		} else {
-			ret[m.Path] = m.Manifest
-		}
+                if _, ok := ret[m.Path]; !ok {
+                        ret[m.Path] = ""
+                }
+                
+                ret[m.Path] += "---\n" + m.Manifest
 	}
 
 	// CRD are not rendered, so do this manually
