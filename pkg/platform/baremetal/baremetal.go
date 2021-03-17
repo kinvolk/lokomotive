00b809aa7f209cba3df06012b9cc0b5bb9facefe
Maartje Eyskens <maartje@kinvolk.io>
2021-03-17 14:20:34 +0100

    baremetal: add certs_validity_period_hours to bare-metal
    This exposes the certs_validity_period_hours option to the bare-metal
    module in lokocfg
    
    Signed-off-by: Maartje Eyskens <maartje@kinvolk.io>

--- pkg/platform/baremetal/baremetal.go
+++ pkg/platform/baremetal/baremetal.go
@@ -62,6 +62,7 @@ type config struct {
 	DownloadProtocol             string              `hcl:"download_protocol,optional"`
 	NetworkIPAutodetectionMethod string              `hcl:"network_ip_autodetection_method,optional"`
 	CLCSnippets                  map[string][]string `hcl:"clc_snippets,optional"`
+	CertsValidityPeriodHours     int                 `hcl:"certs_validity_period_hours,optional"`
 	KubeAPIServerExtraFlags      []string
 }
 
@@ -235,6 +236,7 @@ func createTerraformConfigFile(cfg *config, terraformPath string) error {
 		Labels                       map[string]string
 		EncryptPodTraffic            bool
 		IgnoreX509CNCheck            bool
+		CertsValidityPeriodHours     int
 		ConntrackMaxPerCore          int
 		InstallDisk                  string
 		InstallToSmallestDisk        bool
@@ -266,6 +268,7 @@ func createTerraformConfigFile(cfg *config, terraformPath string) error {
 		Labels:                       cfg.Labels,
 		EncryptPodTraffic:            cfg.EncryptPodTraffic,
 		IgnoreX509CNCheck:            cfg.IgnoreX509CNCheck,
+		CertsValidityPeriodHours:     cfg.CertsValidityPeriodHours,
 		ConntrackMaxPerCore:          cfg.ConntrackMaxPerCore,
 		InstallDisk:                  cfg.InstallDisk,
 		InstallToSmallestDisk:        cfg.InstallToSmallestDisk,
