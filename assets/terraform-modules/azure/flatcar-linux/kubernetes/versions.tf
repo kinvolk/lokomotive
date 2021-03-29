f824d1dfdf6ed078d83b8ca5e8a73ea5fc103331
Maartje Eyskens <maartje@eyskens.me>
2021-03-29 09:23:50 +0200

    Update Azure Terraform Provider to v2.52.0
    Signed-off-by: Maartje Eyskens <maartje@kinvolk.io>

--- assets/terraform-modules/azure/flatcar-linux/kubernetes/versions.tf
+++ assets/terraform-modules/azure/flatcar-linux/kubernetes/versions.tf
@@ -10,7 +10,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.41.0"
+      version = "2.52.0"
     }
     null = {
       source  = "hashicorp/null"
--- assets/terraform-modules/azure/flatcar-linux/kubernetes/workers/versions.tf
+++ assets/terraform-modules/azure/flatcar-linux/kubernetes/workers/versions.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.41.0"
+      version = "2.52.0"
     }
     template = {
       source  = "hashicorp/template"
