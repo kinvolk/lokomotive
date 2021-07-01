079e3170157f70cc127958e4d5e58ada3c588424
Suraj Deshmukh <suraj@kinvolk.io>
2021-07-01 13:15:18 +0530

    Azure TF provider: Update to 2.65.0
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- assets/terraform-modules/azure/flatcar-linux/kubernetes/workers/versions.tf
+++ assets/terraform-modules/azure/flatcar-linux/kubernetes/workers/versions.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.52.0"
+      version = "2.65.0"
     }
     template = {
       source  = "hashicorp/template"
