b5de2f7135887ae78256babccdb414ab9d6ab317
Suraj Deshmukh <suraj@kinvolk.io>
2021-07-28 18:41:40 +0530

    rook-ceph: Move to helm chart
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- pkg/components/rook-ceph/manifests.go
+++ pkg/components/rook-ceph/manifests.go
@@ -14,222 +14,39 @@
 
 package rookceph
 
-// CephCluster resource definition was taken from:
-// https://github.com/rook/rook/blob/v1.6.5/cluster/examples/kubernetes/ceph/cluster.yaml
-var template = map[string]string{
-	"ceph-cluster.yaml": `
-apiVersion: ceph.rook.io/v1
-kind: CephCluster
-metadata:
-  name: rook-ceph
-  namespace: {{ .Namespace }}
-spec:
-  cephVersion:
-    image: ceph/ceph:v15.2.13-20210526
-    allowUnsupported: false
-  dataDirHostPath: /var/lib/rook
-  skipUpgradeChecks: false
-  continueUpgradeAfterChecksEvenIfNotHealthy: false
-  waitTimeoutForHealthyOSDInMinutes: 10
-  mon:
-    count: {{ .MonitorCount }}
-    allowMultiplePerNode: false
-  mgr:
-    count: 2
-    modules:
-    - name: pg_autoscaler
-      enabled: true
-  dashboard:
-    enabled: true
-    ssl: true
-  monitoring:
-    enabled: true
-    rulesNamespace: {{ .Namespace }}
-  network:
-  crashCollector:
-    disable: false
-  cleanupPolicy:
-    confirmation: ""
-    sanitizeDisks:
-      method: quick
-      dataSource: zero
-      iteration: 1
-    allowUninstallWithVolumes: false
-  placement:
-    all:
-      {{- if .NodeAffinity }}
-      nodeAffinity:
-        requiredDuringSchedulingIgnoredDuringExecution:
-          nodeSelectorTerms:
-            - matchExpressions:
-              {{- range $item := .NodeAffinity }}
-              - key: {{ $item.Key }}
-                operator: {{ $item.Operator }}
-                {{- if $item.Values }}
-                values:
-                  {{- range $val := $item.Values }}
-                  - {{ $val }}
-                  {{- end }}
-                {{- end }}
-              {{- end }}
-      {{- end}}
-      {{- if .TolerationsRaw }}
-      tolerations: {{ .TolerationsRaw }}
-      {{- end }}
-  annotations:
-  {{- if .Resources }}
+var chartValuesTmpl = `
+storageClass:
+  enable: {{.StorageClass.Enable}}
+  default: {{.StorageClass.Default}}
+  reclaimPolicy: {{.StorageClass.ReclaimPolicy}}
+
+enableToolbox: {{.EnableToolbox}}
+
+cephCluster:
+  {{if .Resources}}
   resources:
-    mon: {{ .Resources.MONRaw }}
-    mgr: {{ .Resources.MGRRaw }}
-    osd: {{ .Resources.OSDRaw }}
-    mds: {{ .Resources.MDSRaw }}
-    prepareosd: {{ .Resources.PrepareOSDRaw }}
-    crashcollector: {{ .Resources.CrashCollectorRaw }}
-    mgr-sidecar: {{ .Resources.MGRSidecarRaw }}
-  {{- end }}
-  removeOSDsIfOutAndSafeToRemove: false
-  storage: # cluster level storage configuration and selection
-    useAllNodes: true
-    useAllDevices: true
-    config:
-      {{- if .MetadataDevice }}
-      metadataDevice: "{{ .MetadataDevice }}"
-      {{- end }}
-      storeType: bluestore
-      osdsPerDevice: "1" # this value can be overridden at the node or device level
-  disruptionManagement:
-    managePodBudgets: true
-    osdMaintenanceTimeout: 30
-    manageMachineDisruptionBudgets: false
-    machineDisruptionBudgetNamespace: openshift-machine-api
+    mon: {{.Resources.MONRaw}}
+    mgr: {{.Resources.MGRRaw}}
+    osd: {{.Resources.OSDRaw}}
+    mds: {{.Resources.MDSRaw}}
+    prepareosd: {{.Resources.PrepareOSDRaw}}
+    crashcollector: {{.Resources.CrashCollectorRaw}}
+    mgrSidecar: {{.Resources.MGRSidecarRaw}}
+  {{end}}
+  mon:
+    count: {{.MonitorCount}}
+  {{if .MetadataDevice}}
+  metadataDevice: {{.MetadataDevice}}
+  {{end}}
 
-  # healthChecks
-  # Valid values for daemons are 'mon', 'osd', 'status'
-  healthCheck:
-    daemonHealth:
-      mon:
-        disabled: false
-        interval: 45s
-      osd:
-        disabled: false
-        interval: 60s
-      status:
-        disabled: false
-        interval: 60s
-    # Change pod liveness probe, it works for all mon,mgr,osd daemons
-    livenessProbe:
-      mon:
-        disabled: false
-      mgr:
-        disabled: false
-      osd:
-        disabled: false
-`,
+{{if .NodeAffinity}}
+nodeAffinity:
+  requiredDuringSchedulingIgnoredDuringExecution:
+    nodeSelectorTerms:
+    - matchExpressions: {{.NodeAffinityRaw}}
+{{end}}
 
-	"storage-class.yaml": `
-{{- if .StorageClass.Enable }}
-apiVersion: ceph.rook.io/v1
-kind: CephBlockPool
-metadata:
-  name: replicapool
-  namespace: {{ .Namespace }}
-spec:
-  failureDomain: host
-  replicated:
-    size: 3
----
-apiVersion: storage.k8s.io/v1
-kind: StorageClass
-metadata:
-  name: rook-ceph-block
-  annotations:
-    {{- if .StorageClass.Default }}
-    storageclass.kubernetes.io/is-default-class: "true"
-    {{- end }}
-allowVolumeExpansion: true
-provisioner: {{ .Namespace }}.rbd.csi.ceph.com
-parameters:
-  clusterID: {{ .Namespace }}
-  # Ceph pool into which the RBD image shall be created
-  pool: replicapool
-
-  # RBD image format. Defaults to "2".
-  imageFormat: "2"
-
-  # RBD image features. Available for imageFormat: "2". CSI RBD currently supports only 'layering' feature.
-  imageFeatures: layering
-
-  # The secrets contain Ceph admin credentials.
-  csi.storage.k8s.io/provisioner-secret-name: rook-csi-rbd-provisioner
-  csi.storage.k8s.io/provisioner-secret-namespace: {{ .Namespace }}
-  csi.storage.k8s.io/node-stage-secret-name: rook-csi-rbd-node
-  csi.storage.k8s.io/node-stage-secret-namespace: {{ .Namespace }}
-  csi.storage.k8s.io/controller-expand-secret-name: rook-csi-rbd-provisioner
-  csi.storage.k8s.io/controller-expand-secret-namespace: {{ .Namespace }}
-
-  # Specify the filesystem type of the volume. If not specified, csi-provisioner
-  # will set default as 'ext4'.
-  csi.storage.k8s.io/fstype: ext4
-
-# Delete the rbd volume when a PVC is deleted
-reclaimPolicy: {{ .StorageClass.ReclaimPolicy }}
-{{- end }}
-`,
-
-	"ceph-toolbox.yaml": `
-{{- if .EnableToolbox }}
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: rook-ceph-tools
-  namespace: {{ .Namespace }}
-  labels:
-    app: rook-ceph-tools
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: rook-ceph-tools
-  template:
-    metadata:
-      labels:
-        app: rook-ceph-tools
-    spec:
-      dnsPolicy: ClusterFirstWithHostNet
-      containers:
-      - name: rook-ceph-tools
-        image: rook/ceph:v1.6.5
-        command: ["/tini"]
-        args: ["-g", "--", "/usr/local/bin/toolbox.sh"]
-        imagePullPolicy: IfNotPresent
-        env:
-        - name: ROOK_CEPH_USERNAME
-          valueFrom:
-            secretKeyRef:
-              name: rook-ceph-mon
-              key: ceph-username
-        - name: ROOK_CEPH_SECRET
-          valueFrom:
-            secretKeyRef:
-              name: rook-ceph-mon
-              key: ceph-secret
-        volumeMounts:
-        - mountPath: /etc/ceph
-          name: ceph-config
-        - name: mon-endpoint-volume
-          mountPath: /etc/rook
-      volumes:
-      - name: mon-endpoint-volume
-        configMap:
-          name: rook-ceph-mon-endpoints
-          items:
-          - key: data
-            path: mon-endpoints
-      - name: ceph-config
-        emptyDir: {}
-{{- if .TolerationsRaw }}
-      tolerations: {{ .TolerationsRaw }}
-{{- end }}
-{{- end }}
-`,
-}
+{{if .Tolerations}}
+tolerations: {{.TolerationsRaw}}
+{{end}}
+  `
