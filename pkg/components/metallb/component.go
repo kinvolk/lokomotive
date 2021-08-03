b157e46ac7a70de5e39063fd34bec715b97f9eaf
Suraj Deshmukh <suraj@kinvolk.io>
2021-08-03 19:27:59 +0530

    metallb: Enable installation using helm-controller
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- pkg/components/metallb/component.go
+++ pkg/components/metallb/component.go
@@ -16,21 +16,28 @@ package metallb
 
 import (
 	"fmt"
+	"time"
 
 	api "github.com/fluxcd/helm-controller/api/v2beta1"
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/gohcl"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8syaml "sigs.k8s.io/yaml"
 
 	"github.com/kinvolk/lokomotive/internal/template"
 	"github.com/kinvolk/lokomotive/pkg/components"
 	"github.com/kinvolk/lokomotive/pkg/components/util"
 	"github.com/kinvolk/lokomotive/pkg/k8sutil"
+	"github.com/kinvolk/lokomotive/pkg/version"
 )
 
 const (
 	// Name represents MetalLB component name as it should be referenced in function calls
 	// and in configuration.
 	Name = "metallb"
+
+	namespace = "metallb-system"
 )
 
 type component struct {
@@ -59,12 +66,7 @@ func (c *component) LoadConfig(configBody *hcl.Body, evalContext *hcl.EvalContex
 	return gohcl.DecodeBody(*configBody, evalContext, c)
 }
 
-func (c *component) RenderManifests() (map[string]string, error) {
-	helmChart, err := components.Chart(Name)
-	if err != nil {
-		return nil, fmt.Errorf("retrieving chart from assets: %w", err)
-	}
-
+func (c *component) generateValues() (string, error) {
 	// Here are `nodeSelectors` and `tolerations` that are set by upstream. To make sure that we
 	// don't miss them out we set them manually here. We cannot make these changes in the template
 	// because we have parameterized these fields.
@@ -88,19 +90,28 @@ func (c *component) RenderManifests() (map[string]string, error) {
 
 	t, err := util.RenderTolerations(c.SpeakerTolerations)
 	if err != nil {
-		return nil, fmt.Errorf("marshaling speaker tolerations: %w", err)
+		return "", fmt.Errorf("marshaling speaker tolerations: %w", err)
 	}
 	c.SpeakerTolerationsJSON = t
 
 	t, err = util.RenderTolerations(c.ControllerTolerations)
 	if err != nil {
-		return nil, fmt.Errorf("rendering controller tolerations: %w", err)
+		return "", fmt.Errorf("rendering controller tolerations: %w", err)
 	}
 	c.ControllerTolerationsJSON = t
 
-	values, err := template.Render(chartValuesTmpl, c)
+	return template.Render(chartValuesTmpl, c)
+}
+
+func (c *component) RenderManifests() (map[string]string, error) {
+	values, err := c.generateValues()
 	if err != nil {
 		return nil, fmt.Errorf("rendering values template failed: %w", err)
+	}
+
+	helmChart, err := components.Chart(Name)
+	if err != nil {
+		return nil, fmt.Errorf("retrieving chart from assets: %w", err)
 	}
 
 	renderedFiles, err := util.RenderChart(helmChart, Name, c.Metadata().Namespace.Name, values)
@@ -111,15 +122,58 @@ func (c *component) RenderManifests() (map[string]string, error) {
 	return renderedFiles, nil
 }
 
+func (c *component) GenerateHelmRelease() (*api.HelmRelease, error) {
+	valuesYaml, err := c.generateValues()
+	if err != nil {
+		return nil, fmt.Errorf("rendering values template failed: %w", err)
+	}
+
+	values, err := k8syaml.YAMLToJSON([]byte(valuesYaml))
+	if err != nil {
+		return nil, fmt.Errorf("converting YAML to JSON: %w", err)
+	}
+
+	interval := time.Minute * 10
+
+	return &api.HelmRelease{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      Name,
+			Namespace: "flux-system",
+		},
+		Spec: api.HelmReleaseSpec{
+			Chart: api.HelmChartTemplate{
+				Spec: api.HelmChartTemplateSpec{
+					Chart: "./assets/charts/components/metallb/",
+					SourceRef: api.CrossNamespaceObjectReference{
+						Kind: "GitRepository",
+						Name: "lokomotive-" + version.Version,
+					},
+				},
+			},
+			ReleaseName: Name,
+			Install: &api.Install{
+				CRDs:            api.CreateReplace,
+				CreateNamespace: true,
+			},
+			Upgrade: &api.Upgrade{
+				Force: true,
+				CRDs:  api.CreateReplace,
+			},
+			Interval:        metav1.Duration{time.Minute},
+			Timeout:         &metav1.Duration{interval},
+			TargetNamespace: namespace,
+			Values: &apiextensionsv1.JSON{
+				Raw: values,
+			},
+		},
+	}, nil
+}
+
 func (c *component) Metadata() components.Metadata {
 	return components.Metadata{
 		Name: Name,
 		Namespace: k8sutil.Namespace{
-			Name: "metallb-system",
+			Name: namespace,
 		},
 	}
-}
-
-func (c *component) GenerateHelmRelease() (*api.HelmRelease, error) {
-	return nil, components.NotImplementedErr
 }
