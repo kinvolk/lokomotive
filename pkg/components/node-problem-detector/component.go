a7fd8ce335a8a18950414510818f540edf19d669
Suraj Deshmukh <suraj@kinvolk.io>
2021-08-13 16:36:36 +0530

    node-problem-detector: Enable installation using helm-controller
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- pkg/components/node-problem-detector/component.go
+++ pkg/components/node-problem-detector/component.go
@@ -21,11 +21,15 @@ import (
 	helmcontrollerapi "github.com/fluxcd/helm-controller/api/v2beta1"
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/gohcl"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8syaml "sigs.k8s.io/yaml"
 
 	"github.com/kinvolk/lokomotive/internal/template"
 	"github.com/kinvolk/lokomotive/pkg/components"
 	"github.com/kinvolk/lokomotive/pkg/components/util"
 	"github.com/kinvolk/lokomotive/pkg/k8sutil"
+	"github.com/kinvolk/lokomotive/pkg/version"
 )
 
 const (
@@ -94,5 +98,48 @@ func (c *component) Metadata() components.Metadata {
 }
 
 func (c *component) GenerateHelmRelease() (*helmcontrollerapi.HelmRelease, error) {
-	return nil, components.ErrNotImplemented
+	valuesYaml, err := template.Render(chartValuesTmpl, c)
+	if err != nil {
+		return nil, fmt.Errorf("rendering chart values template: %w", err)
+	}
+
+	values, err := k8syaml.YAMLToJSON([]byte(valuesYaml))
+	if err != nil {
+		return nil, fmt.Errorf("converting YAML to JSON: %w", err)
+	}
+
+	return &helmcontrollerapi.HelmRelease{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      Name,
+			Namespace: "flux-system",
+		},
+		Spec: helmcontrollerapi.HelmReleaseSpec{
+			Chart: helmcontrollerapi.HelmChartTemplate{
+				Spec: helmcontrollerapi.HelmChartTemplateSpec{
+					Chart: components.ComponentsPath + Name,
+					SourceRef: helmcontrollerapi.CrossNamespaceObjectReference{
+						Kind: "GitRepository",
+						Name: "lokomotive-" + version.Version,
+					},
+				},
+			},
+			ReleaseName: Name,
+			Install: &helmcontrollerapi.Install{
+				CRDs:            helmcontrollerapi.CreateReplace,
+				CreateNamespace: true,
+				Remediation: &helmcontrollerapi.InstallRemediation{
+					Retries: -1,
+				},
+			},
+			Upgrade: &helmcontrollerapi.Upgrade{
+				CRDs: helmcontrollerapi.CreateReplace,
+			},
+			Interval:        components.FluxInstallInterval,
+			Timeout:         &components.FluxInstallTimeout,
+			TargetNamespace: c.Namespace,
+			Values: &apiextensionsv1.JSON{
+				Raw: values,
+			},
+		},
+	}, nil
 }
